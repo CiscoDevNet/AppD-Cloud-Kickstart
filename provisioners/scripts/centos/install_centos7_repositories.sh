ad86f471d6e294dd41efc3287f8bd0c1cdae100e
Ed Barberis <ed.barberis@gmail.com>
2019-06-17 15:55:27 -0400

    Added support for bash completion.

--- provisioners/scripts/centos/install_centos7_repositories.sh
+++ provisioners/scripts/centos/install_centos7_repositories.sh
@@ -4,6 +4,7 @@
 # install useful base utilities. -------------------------------------------------------------------
 yum -y install yum-utils bind-utils unzip vim-enhanced tree bc
 yum -y install openssh-clients sudo kernel-headers kernel-devel gcc make perl selinux-policy-devel wget nfs-utils net-tools bzip2
+yum -y install bash-completion bash-completion-extras
 
 # install the latest ol7 updates. ------------------------------------------------------------------
 yum -y update
