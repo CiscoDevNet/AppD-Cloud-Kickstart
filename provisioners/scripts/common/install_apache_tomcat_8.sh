3e65b864c4204e9b131be6036fb1ab743183999d
Ed Barberis <ed.barberis@gmail.com>
2019-08-22 12:54:52 -0400

    Upgraded support for Apache Tomcat 8.5.45.

--- provisioners/scripts/common/install_apache_tomcat_8.sh
+++ provisioners/scripts/common/install_apache_tomcat_8.sh
@@ -5,7 +5,7 @@
 # set default values for input environment variables if not set. ---------------
 # tomcat web server install parameters.
 tomcat_home="${tomcat_home:-apache-tomcat-8}"                       # [optional] tomcat home (defaults to 'apache-tomcat-8').
-tomcat_release="${tomcat_release:-8.5.43}"                          # [optional] tomcat release (defaults to '8.5.43').
+tomcat_release="${tomcat_release:-8.5.45}"                          # [optional] tomcat release (defaults to '8.5.45').
 
 tomcat_username="${tomcat_username:-centos}"                        # [optional] tomcat user name (defaults to 'centos').
 tomcat_group="${tomcat_group:-centos}"                              # [optional] tomcat group (defaults to 'centos').
@@ -30,7 +30,7 @@ Usage:
   Example:
    # tomcat web server install parameters.
     [root]# export tomcat_home="apache-tomcat-8"                # [optional] tomcat home (defaults to 'apache-tomcat-8').
-    [root]# export tomcat_release="8.5.43"                      # [optional] tomcat release (defaults to '8.5.43').
+    [root]# export tomcat_release="8.5.45"                      # [optional] tomcat release (defaults to '8.5.45').
    #
     [root]# export tomcat_username="centos"                     # [optional] tomcat user name (defaults to 'centos').
     [root]# export tomcat_group="centos"                        # [optional] tomcat group (defaults to 'centos').
