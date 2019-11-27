93267d6d35ee01ff62b4fff7b74384af66affdb5
Ed Barberis <ed.barberis@gmail.com>
2019-11-26 19:20:38 -0500

    Upgraded AppDynamics Enterprise Console to 4.5.16.21269.

--- provisioners/scripts/centos/install_centos7_appdynamics_enterprise_console.sh
+++ provisioners/scripts/centos/install_centos7_appdynamics_enterprise_console.sh
@@ -29,7 +29,7 @@ set -x  # turn command display back ON.
 # appd platform install parameters.
 appd_home="${appd_home:-/opt/appdynamics}"
 appd_platform_home="${appd_platform_home:-platform}"
-appd_platform_release="${appd_platform_release:-4.5.16.21250}"
+appd_platform_release="${appd_platform_release:-4.5.16.21269}"
 set +x  # temporarily turn command display OFF.
 appd_platform_admin_username="${appd_platform_admin_username:-admin}"
 appd_platform_admin_password="${appd_platform_admin_password:-welcome1}"
@@ -62,7 +62,7 @@ Usage:
   [OPTIONAL] appdynamics platform install parameters [w/ defaults].
     [root]# export appd_home="/opt/appdynamics"                         # [optional] appd home (defaults to '/opt/appdynamics').
     [root]# export appd_platform_home="platform"                        # [optional] platform home folder (defaults to 'platform').
-    [root]# export appd_platform_release="4.5.16.21250"                 # [optional] platform release (defaults to '4.5.16.21250').
+    [root]# export appd_platform_release="4.5.16.21269"                 # [optional] platform release (defaults to '4.5.16.21269').
     [root]# export appd_platform_admin_username="admin"                 # [optional] platform admin user name (defaults to user 'admin').
     [root]# export appd_platform_admin_password="welcome1"              # [optional] platform admin password (defaults to 'welcome1').
     [root]# export appd_platform_db_password="welcome1"                 # [optional] platform database password (defaults to 'welcome1').
