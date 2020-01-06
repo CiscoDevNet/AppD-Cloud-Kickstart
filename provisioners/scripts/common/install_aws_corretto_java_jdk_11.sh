b6fc9c4e3d82d5630b3af2360a8c6970c0ccc491
Ed Barberis <ed.barberis@gmail.com>
2020-01-06 16:23:59 -0500

    Refactored download URLs for Amazon Corretto JDK 8.232 and 11.0.5.

--- provisioners/scripts/common/install_aws_corretto_java_jdk_11.sh
+++ provisioners/scripts/common/install_aws_corretto_java_jdk_11.sh
@@ -4,17 +4,18 @@
 # install amazon corretto 11. ----------------------------------------------------------------------
 jdkhome="jdk11"
 jdkbuild="11.0.5.10.1"
-jdkhost="d3pxv6yz143wms"
 #jdkchecksum="d732b6ece7b1c8117753ba8460dfaede"
 jdkfolder="amazon-corretto-${jdkbuild}-linux-x64"
 jdkbinary="amazon-corretto-${jdkbuild}-linux-x64.tar.gz"
+#jdkbinary="amazon-corretto-${jdkbuild:0:2}-x64-linux-jdk.tar.gz"
 
 # create java home parent folder.
 mkdir -p /usr/local/java
 cd /usr/local/java
 
 # download corretto 11 binary from aws.
-wget --no-verbose https://${jdkhost}.cloudfront.net/${jdkbuild}/${jdkbinary}
+wget --no-verbose https://corretto.aws/downloads/resources/${jdkbuild}/${jdkbinary}
+#wget --no-verbose https://corretto.aws/downloads/latest/${jdkbinary}
 
 # extract corretto 11 binary and create softlink to 'jdk11'.
 rm -f ${jdkhome}
--- provisioners/scripts/common/install_aws_corretto_java_jdk_8.sh
+++ provisioners/scripts/common/install_aws_corretto_java_jdk_8.sh
@@ -4,17 +4,18 @@
 # install amazon corretto 8. -----------------------------------------------------------------------
 jdkhome="jdk180"
 jdkbuild="8.232.09.1"
-jdkhost="d3pxv6yz143wms"
 #jdkchecksum="3511152bd52c867f8b550d7c8d7764aa"
 jdkfolder="amazon-corretto-${jdkbuild}-linux-x64"
 jdkbinary="amazon-corretto-${jdkbuild}-linux-x64.tar.gz"
+#jdkbinary="amazon-corretto-${jdkbuild:0:1}-x64-linux-jdk.tar.gz"
 
 # create java home parent folder.
 mkdir -p /usr/local/java
 cd /usr/local/java
 
 # download corretto 8 binary from aws.
-wget --no-verbose https://${jdkhost}.cloudfront.net/${jdkbuild}/${jdkbinary}
+wget --no-verbose https://corretto.aws/downloads/resources/${jdkbuild}/${jdkbinary}
+#wget --no-verbose https://corretto.aws/downloads/latest/${jdkbinary}
 
 # extract corretto 8 binary and create softlink to 'jdk180'.
 rm -f ${jdkhome}
