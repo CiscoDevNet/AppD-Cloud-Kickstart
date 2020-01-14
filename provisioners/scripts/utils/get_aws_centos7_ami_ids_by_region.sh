306071895c23d9919eb0e08fadcf604211bb03fe
Ed Barberis <ed.barberis@gmail.com>
2020-01-14 10:15:22 -0500

    Added support for the AWS region in São Paulo.

--- provisioners/scripts/utils/get_aws_centos7_ami_ids_by_region.sh
+++ provisioners/scripts/utils/get_aws_centos7_ami_ids_by_region.sh
@@ -16,8 +16,8 @@ echo "# CentOS 7.7 AMI Image IDs as of: ${curdate}."
 
 # loop for each aws region. ------------------------------------------------------------------------
 # initialize aws regions for north america and europe.
-aws_region_ids_array=( "ap-southeast-2" "ca-central-1" "eu-central-1" "eu-west-1" "eu-west-2" "eu-west-3" "us-east-1" "us-east-2" "us-west-1" "us-west-2" )
-aws_region_names_array=( "Sydney" "Central" "Frankfurt" "Ireland" "London" "Paris" "N. Virginia" "Ohio" "N. California" "Oregon" )
+aws_region_ids_array=( "ap-southeast-2" "ca-central-1" "eu-central-1" "eu-west-1" "eu-west-2" "eu-west-3" "sa-east-1" "us-east-1" "us-east-2" "us-west-1" "us-west-2" )
+aws_region_names_array=( "Sydney" "Central" "Frankfurt" "Ireland" "London" "Paris" "São Paulo" "N. Virginia" "Ohio" "N. California" "Oregon" )
 
 # initialize array index.
 ii=0
@@ -33,13 +33,14 @@ done
 echo "#export aws_ami_source"
 
 # output will look like the following: -------------------------------------------------------------
-# CentOS 7.7 AMI Image IDs as of: 13 Jan 2020.
+# CentOS 7.7 AMI Image IDs as of: 14 Jan 2020.
 #aws_ami_source="ami-08bd00d7713a39e7d"     # CentOS 7.7 AMI (HVM), SSD Volume Type: ap-southeast-2 [Sydney]
 #aws_ami_source="ami-033e6106180a626d0"     # CentOS 7.7 AMI (HVM), SSD Volume Type: ca-central-1 [Central]
 #aws_ami_source="ami-04cf43aca3e6f3de3"     # CentOS 7.7 AMI (HVM), SSD Volume Type: eu-central-1 [Frankfurt]
 #aws_ami_source="ami-0ff760d16d9497662"     # CentOS 7.7 AMI (HVM), SSD Volume Type: eu-west-1 [Ireland]
 #aws_ami_source="ami-0eab3a90fc693af19"     # CentOS 7.7 AMI (HVM), SSD Volume Type: eu-west-2 [London]
 #aws_ami_source="ami-0e1ab783dc9489f34"     # CentOS 7.7 AMI (HVM), SSD Volume Type: eu-west-3 [Paris]
+#aws_ami_source="ami-0b8d86d4bf91850af"     # CentOS 7.7 AMI (HVM), SSD Volume Type: sa-east-1 [São Paulo]
 #aws_ami_source="ami-02eac2c0129f6376b"     # CentOS 7.7 AMI (HVM), SSD Volume Type: us-east-1 [N. Virginia]
 #aws_ami_source="ami-0f2b4fc905b0bd1f1"     # CentOS 7.7 AMI (HVM), SSD Volume Type: us-east-2 [Ohio]
 #aws_ami_source="ami-074e2d6769f445be5"     # CentOS 7.7 AMI (HVM), SSD Volume Type: us-west-1 [N. California]
