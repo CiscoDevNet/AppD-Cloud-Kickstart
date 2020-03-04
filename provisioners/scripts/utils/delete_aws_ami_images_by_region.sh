3f366c0e7601388f5d2a9c4f2dceae2702b4ea3a
Ed Barberis <ed.barberis@gmail.com>
2020-03-04 11:54:03 -0600

    Upgraded Homebrew to v2.2.9.

--- provisioners/scripts/utils/delete_aws_ami_images_by_region.sh
+++ provisioners/scripts/utils/delete_aws_ami_images_by_region.sh
@@ -10,7 +10,7 @@
 
 # set default values for input environment variables if not set. -----------------------------------
 appd_platform_name="${appd_platform_name-AppDynamics Cloud Kickstart}"
-appd_project_image_types="${appd_project_image_types-APM-Platform-CentOS CWOM-Platform-CentOS EXT-CentOS LPAD-CentOS K8S-CentOS DEVNET-CentOS}"
+appd_project_image_types="${appd_project_image_types-APM-Platform-CentOS CWOM-Platform-CentOS EXT-CentOS LPAD-AL2 LPAD-CentOS K8S-CentOS DEVNET-CentOS}"
 aws_ami_region="${aws_ami_region-us-east-1}"
 aws_ami_keep_last="${aws_ami_keep_last-true}"
 
