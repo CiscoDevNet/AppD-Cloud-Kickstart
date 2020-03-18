c5d18bbdd5ad03baf255d0e85dd63596740dd376
Ed Barberis <ed.barberis@gmail.com>
2020-03-18 17:32:38 -0400

    Added support for specific AWS availability zones and Kubernetes versions.

--- applications/aws/AD-Capital-Kube/create_eks_cluster.sh
+++ applications/aws/AD-Capital-Kube/create_eks_cluster.sh
@@ -1,61 +1,68 @@
 #!/bin/sh -eux
 #---------------------------------------------------------------------------------------------------
-# Create an AWS EKS cluster with eksctl utility
-# 
-# export appd_aws_eks_user_name=User-0X
-# export appd_aws_eks_region=us-east-1
-# 
+# Create an AWS EKS cluster with eksctl utility from Weaveworks.
+#
+# eksctl is a simple CLI tool for creating clusters on EKS, Amazon’s managed Kubernetes service
+# for EC2. It is written in Go, and uses CloudFormation.
+#
 # For more details, please visit:
+#   https://eksctl.io/
 #   https://github.com/weaveworks/eksctl
 #
 # NOTE: All inputs are defined by external environment variables.
 #       Optional variables have reasonable defaults, but you may override as needed.
 #       See 'usage()' function below for environment variable descriptions.
-#       Script should be run with 'root' privilege.
 #---------------------------------------------------------------------------------------------------
 
 # set default values for input environment variables if not set. -----------------------------------
-# [MANDATORY] appdynamics account parameters.
-set +x  # temporarily turn command display OFF.
+# [MANDATORY] eksctl command parameters.
 appd_aws_eks_user_name="${appd_aws_eks_user_name:-}"
 appd_aws_eks_region="${appd_aws_eks_region:-}"
-set -x  # turn command display back ON.
+
+# [OPTIONAL] eksctl command parameters [w/ defaults].
+appd_aws_eks_zones="${appd_aws_eks_zones:-}"
+appd_aws_eks_version="${appd_aws_eks_version:-1.14}"
+appd_aws_eks_name="${appd_aws_eks_name:-AD-Capital-${appd_aws_eks_user_name}}"
+appd_aws_eks_environment="${appd_aws_eks_environment:-Lab-Env-${appd_aws_eks_user_name}}"
+appd_aws_eks_nodes="${appd_aws_eks_nodes:-2}"
+appd_aws_eks_node_type="${appd_aws_eks_node_type:-m5a.large}"
+appd_aws_eks_ssh_public_key="${appd_aws_eks_ssh_public_key:-AppD-Cloud-Kickstart-AWS}"
 
 # define usage function. ---------------------------------------------------------------------------
 usage() {
   cat <<EOF
 Usage:
-  Create an AWS EKS cluster with eksctl utility
+  Create an AWS EKS cluster with eksctl utility from Weaveworks.
 
   NOTE: All inputs are defined by external environment variables.
         Optional variables have reasonable defaults, but you may override as needed.
-        Script should be run with 'root' privilege.
 
   -------------------------------------
   Description of Environment Variables:
   -------------------------------------
-  [MANDATORY] eksctl command parameters
-    [root]# export appd_aws_eks_user_name="User-0X"                        # lab participant user name
-    [root]# export appd_aws_eks_region="us-east-1"                         # AWS region (us-east-1 or us-east-2)
+  [MANDATORY] eksctl command parameters.
+    [centos]$ export appd_aws_eks_user_name="Lab-User-01"                   # lab participant user name.
+    [centos]$ export appd_aws_eks_region="us-east-1"                        # aws region (us-east-1 or us-east-2).
 
   [OPTIONAL] eksctl command parameters [w/ defaults].
-    [root]# export appd_aws_eks_name="AD-Capital-User-0X"                  # [optional] eks cluster name (defaults to 'AD-Capital-${appd_aws_eks_user_name}')
-    [root]# export appd_aws_eks_environment="Lab-Env-User-0X"              # [optional] eks environment decription (defaults to 'Lab-Env-${appd_aws_eks_user_name}')
-    [root]# export appd_aws_eks_nodes="2"                                  # [optional] number of eks worker nodes (defaults to '2')
-    [root]# export appd_aws_eks_node_type="m5a.large"                      # [optional] type of EC2 instance for nodes (defaults to user 'm5a.large')
-    [root]# export appd_aws_eks_ssh_public_key="AppD-Cloud-Kickstart-AWS"  # [optional] public key used to ssh to eks cluster (defaults to 'AppD-Cloud-Kickstart-AWS')
+    [centos]$ export appd_aws_eks_zones="us-east-1a,us-east-1b,us-east-1c"  # [optional] aws availability zones.
+    [centos]$ export appd_aws_eks_version="1.15"                            # [optional] kubernetes version (defaults to '1.14').
+                                                                            #            valid versions:
+                                                                            #              '1.13', '1.14', '1.15'
+    [centos]$ export appd_aws_eks_name="AD-Capital-Lab-User-01"             # [optional] eks cluster name (defaults to 'AD-Capital-${appd_aws_eks_user_name}')
+    [centos]$ export appd_aws_eks_environment="Lab-Env-Lab-User-01"         # [optional] eks environment decription (defaults to 'Lab-Env-${appd_aws_eks_user_name}')
+    [centos]$ export appd_aws_eks_nodes="2"                                 # [optional] number of eks worker nodes (defaults to '2')
+    [centos]$ export appd_aws_eks_node_type="m5a.large"                     # [optional] type of ec2 instance for nodes (defaults to user 'm5a.large')
+    [centos]$ export appd_aws_eks_ssh_public_key="AppD-Cloud-Kickstart-AWS" # [optional] public key used to ssh to eks cluster (defaults to 'AppD-Cloud-Kickstart-AWS')
 
   --------
   Example:
   --------
-    [root]# $0
+    [centos]$ $0
 EOF
 }
 
-
-# validate mandatory environment variables. --------------------------------------------------
-set +x  # temporarily turn command display OFF.
-
+# validate environment variables. ------------------------------------------------------------------
 if [ -z "$appd_aws_eks_user_name" ]; then
   echo "Error: 'appd_aws_eks_user_name' environment variable not set."
   usage
@@ -68,22 +75,24 @@ if [ -z "$appd_aws_eks_region" ]; then
   exit 1
 fi
 
-set -x  # turn command display back ON.
+if [ -n "$appd_aws_eks_version" ]; then
+  case $appd_aws_eks_version in
+      1.13|1.14|1.15)
+        ;;
+      *)
+        echo "Error: invalid 'appd_aws_eks_version'."
+        usage
+        exit 1
+        ;;
+  esac
+fi
 
+# populate 'zones' option if set. ------------------------------------------------------------------
+if [ -z "$appd_aws_eks_zones" ]; then
+  appd_aws_eks_zones_option=""
+else
+  appd_aws_eks_zones_option="--zones=${appd_aws_eks_zones}"
+fi
 
-# set optional environment variables. --------------------------------------------------------
-set +x  # temporarily turn command display OFF.
-
-appd_aws_eks_name="AD-Capital-${appd_aws_eks_user_name}"
-appd_aws_eks_environment="Lab-Env-${appd_aws_eks_user_name}"
-appd_aws_eks_nodes="${appd_aws_eks_nodes:-2}"
-appd_aws_eks_node_type="${appd_aws_eks_node_type:-m5a.large}"
-appd_aws_eks_ssh_public_key="${appd_aws_eks_ssh_public_key:-AppD-Cloud-Kickstart-AWS}"
-
-# run eksctl command to create the eks cluster. ----------------------------------------------
-#echo "eksctl create cluster --name="${appd_aws_eks_name}" --region="${appd_aws_eks_region}" --tags environment="${appd_aws_eks_environment}" --nodes="${appd_aws_eks_nodes}" --node-type="${appd_aws_eks_node_type}" --node-ami=auto --ssh-access --ssh-public-key="${appd_aws_eks_ssh_public_key}
-
-set -x  # turn command display back ON.
-
-eksctl create cluster --name=${appd_aws_eks_name} --region=${appd_aws_eks_region} --tags environment=${appd_aws_eks_environment} --nodes=${appd_aws_eks_nodes} --node-type=${appd_aws_eks_node_type} --node-ami=auto --ssh-access --ssh-public-key=${appd_aws_eks_ssh_public_key}
-
+# run eksctl command to create the eks cluster. ----------------------------------------------------
+eksctl create cluster --name=${appd_aws_eks_name} --region=${appd_aws_eks_region} ${appd_aws_eks_zones_option} --version=${appd_aws_eks_version} --tags environment=${appd_aws_eks_environment} --nodes=${appd_aws_eks_nodes} --node-type=${appd_aws_eks_node_type} --node-ami=auto --ssh-access --ssh-public-key=${appd_aws_eks_ssh_public_key}
