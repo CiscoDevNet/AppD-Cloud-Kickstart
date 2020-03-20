48a632a55816aec04e0f9c0485f31108287874ed
Ed Barberis <ed.barberis@gmail.com>
2020-03-20 16:19:38 -0400

    Added SHA256 support for Oracle Java SE JDK 13.0.2 and 14.

--- provisioners/scripts/common/install_oracle_java_jdk_14.sh
+++ provisioners/scripts/common/install_oracle_java_jdk_14.sh
@@ -2,28 +2,33 @@
 # install java se 14 development kit by oracle.
 
 # install java se 14 development kit. --------------------------------------------------------------
-jdkhome="jdk14"
-jdkbuild="14+36"
-jdkhash="076bab302c7b4508975440c56f6cc26a"
-jdkfolder="jdk-14"
-jdkbinary="${jdkfolder}_linux-x64_bin.tar.gz"
+jdk_home="jdk14"
+jdk_build="14+36"
+jdk_hash="076bab302c7b4508975440c56f6cc26a"
+jdk_folder="jdk-14"
+jdk_binary="${jdk_folder}_linux-x64_bin.tar.gz"
+jdk_sha256="4639bbaecc9cc606f1a4b99fda1efcaefcbf57a7025b3828b095093a6c866afd"
 
 # create java home parent folder.
 mkdir -p /usr/local/java
 cd /usr/local/java
 
 # download jdk 14 binary from oracle otn.
-wget --no-verbose --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" https://download.oracle.com/otn-pub/java/jdk/${jdkbuild}/${jdkhash}/${jdkbinary}
+wget --no-verbose --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" https://download.oracle.com/otn-pub/java/jdk/${jdk_build}/${jdk_hash}/${jdk_binary}
+
+# verify the downloaded binary.
+echo "${jdk_sha256} ${jdk_binary}" | sha256sum --check
+# ${jdk_folder}_linux-x64_bin.tar.gz: OK
 
 # extract jdk 14 binary and create softlink to 'jdk14'.
-rm -f ${jdkhome}
-tar -zxvf ${jdkbinary} --no-same-owner --no-overwrite-dir
-chown -R root:root ./${jdkfolder}
-ln -s ${jdkfolder} ${jdkhome}
-rm -f ${jdkbinary}
+rm -f ${jdk_home}
+tar -zxvf ${jdk_binary} --no-same-owner --no-overwrite-dir
+chown -R root:root ./${jdk_folder}
+ln -s ${jdk_folder} ${jdk_home}
+rm -f ${jdk_binary}
 
 # set jdk 14 home environment variables.
-JAVA_HOME=/usr/local/java/${jdkhome}
+JAVA_HOME=/usr/local/java/${jdk_home}
 export JAVA_HOME
 PATH=${JAVA_HOME}/bin:$PATH
 export PATH
