2361de1a0ac6b5bc30ac0e5db2614883280947d5
Ed Barberis <ed.barberis@gmail.com>
2020-04-15 18:52:53 -0400

    Upgraded support for Apache Tomcat 8.5.54.

--- provisioners/scripts/centos/install_centos7_apache_tomcat_8.sh
+++ provisioners/scripts/centos/install_centos7_apache_tomcat_8.sh
@@ -7,7 +7,7 @@
 # set default values for input environment variables if not set. -----------------------------------
 # tomcat web server install parameters.
 tomcat_home="${tomcat_home:-apache-tomcat-8}"                       # [optional] tomcat home (defaults to 'apache-tomcat-8').
-tomcat_release="${tomcat_release:-8.5.53}"                          # [optional] tomcat release (defaults to '8.5.53').
+tomcat_release="${tomcat_release:-8.5.54}"                          # [optional] tomcat release (defaults to '8.5.54').
 
 tomcat_username="${tomcat_username:-centos}"                        # [optional] tomcat user name (defaults to 'centos').
 tomcat_group="${tomcat_group:-centos}"                              # [optional] tomcat group (defaults to 'centos').
@@ -27,7 +27,7 @@ tomcat_java_opts="${tomcat_java_opts:--Djava.awt.headless=true -Djava.security.e
 # set tomcat web server installation variables.
 tomcat_folder="${tomcat_home:0:-2}-${tomcat_release}"
 tomcat_binary="${tomcat_folder}.tar.gz"
-tomcat_sha512="849b4f1a4c5609715f29dec3089d75ed500cf87acc5882e7b2ac469bc86915f86cf30a2518d3855ca083d0946ad50d348f70dff0d56cefa3724e2083a08c8636"
+tomcat_sha512="dc3ba0183d1e6f6006e5430e91abbc0758bdb719efebea5809f7034c9a28c590a764d8fcf2528a27a98cb86531e94cd9dc5a92cdbcae36e808ced0e5f1c665b8"
 
 # create apache parent folder.
 mkdir -p /usr/local/apache
