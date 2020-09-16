e0781440248ea80f248207f40a5fe2ac98c3476f
Ed Barberis <ed.barberis@gmail.com>
2020-09-16 13:33:19 -0400

    Upgraded support for Apache Tomcat 8.5.58.

--- provisioners/scripts/centos/install_centos7_apache_tomcat_8.sh
+++ provisioners/scripts/centos/install_centos7_apache_tomcat_8.sh
@@ -7,9 +7,9 @@
 # set default values for input environment variables if not set. -----------------------------------
 # tomcat web server install parameters.
 tomcat_home="${tomcat_home:-apache-tomcat-8}"                       # [optional] tomcat home (defaults to 'apache-tomcat-8').
-tomcat_release="${tomcat_release:-8.5.57}"                          # [optional] tomcat release (defaults to '8.5.57').
+tomcat_release="${tomcat_release:-8.5.58}"                          # [optional] tomcat release (defaults to '8.5.58').
                                                                     # [optional] tomcat sha-512 checksum (defaults to published value).
-tomcat_sha512="${tomcat_sha512:-720de36bb3e40a4c67bdf0137b12ae0fd733aef772d81a4b8dab00f29924ddd17ecb2a7217b9551fc0ca51bd81d1da13ad63b6694c445e5c0e42dfa7f279ede1}"
+tomcat_sha512="${tomcat_sha512:-55d8fa698f7ac118ab33f1be644477fa8424f0fe6eefa80c85acd4e3cbce5f1704ce3cf897dfcd42c5c95cd2ff3b559e774fb5b7ac7279dd6b803a9a2dd8cc8f}"
 
 tomcat_username="${tomcat_username:-centos}"                        # [optional] tomcat user name (defaults to 'centos').
 tomcat_group="${tomcat_group:-centos}"                              # [optional] tomcat group (defaults to 'centos').
