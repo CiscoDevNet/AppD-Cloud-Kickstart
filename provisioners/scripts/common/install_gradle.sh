f7fc72fe29d0747863fcd3d33f0eba218452620b
Ed Barberis <ed.barberis@gmail.com>
2020-10-23 14:57:31 -0400

    Upgraded Gradle to version 6.7.

--- provisioners/scripts/common/install_gradle.sh
+++ provisioners/scripts/common/install_gradle.sh
@@ -11,10 +11,10 @@ rm -f gradle-release-notes.html
 curl --silent https://docs.gradle.org/current/release-notes.html --output gradle-release-notes.html
 gradle_home="gradle"
 gradle_release=$(awk '/Release Notes<\/title>/ {print $2}' gradle-release-notes.html)
-gradle_release="6.6.1"
+gradle_release="6.7"
 gradle_folder="gradle-${gradle_release}"
 gradle_binary="gradle-${gradle_release}-all.zip"
-gradle_sha256="11657af6356b7587bfb37287b5992e94a9686d5c8a0a1b60b87b9928a2decde5"
+gradle_sha256="0080de8491f0918e4f529a6db6820fa0b9e818ee2386117f4394f95feb1d5583"
 
 # download gradle build tool from gradle.org.
 curl --silent --location https://services.gradle.org/distributions/${gradle_binary} --output ${gradle_binary}
