d033f11e190bcc7f1ede14daeebea697bd2be24c
Ed Barberis <ed.barberis@gmail.com>
2020-10-26 08:43:12 -0400

    Upgraded support for Amazon Corretto JDK 15.0.1.

--- provisioners/scripts/common/install_aws_corretto_java_jdk_15.sh
+++ provisioners/scripts/common/install_aws_corretto_java_jdk_15.sh
@@ -18,8 +18,8 @@
 
 # set amazon corretto 15 installation variables. ---------------------------------------------------
 jdk_home="jdk15"
-jdk_build="15.0.0.36.1"
-jdk_checksum="876aa222c3271cfcc8f19f8610ec8310"
+jdk_build="15.0.1.9.1"
+jdk_checksum="94954e91b04f4b98f4f34a4cec4520c6"
 jdk_folder="amazon-corretto-${jdk_build}-linux-x64"
 jdk_binary="amazon-corretto-${jdk_build}-linux-x64.tar.gz"
 #jdk_binary="amazon-corretto-${jdk_build:0:2}-x64-linux-jdk.tar.gz"
