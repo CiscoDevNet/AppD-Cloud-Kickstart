904bafd29576f4991d74670a7b3538f76d547821
Ed Barberis <ed.barberis@gmail.com>
2020-11-19 13:27:49 -0500

    Added support for HashiCorp Packer v1.6.5 and Terraform v0.13.5.

---
+++ provisioners/scripts/common/install_hashicorp_packer.sh
@@ -0,0 +1,51 @@
+#!/bin/sh -eux
+#---------------------------------------------------------------------------------------------------
+# Install Packer Machine and Container Image Tool by HashiCorp.
+#
+# Packer is an open source tool for creating identical machine images for multiple platforms from
+# a single source configuration. Packer is lightweight, runs on every major operating system, and
+# is highly performant, creating machine images for multiple platforms in parallel. Packer does
+# not replace configuration management like Chef or Puppet. In fact, when building images, Packer
+# is able to use tools like Chef or Puppet to install software onto the image.
+#
+# A machine image is a single static unit that contains a pre-configured operating system and
+# installed software which is used to quickly create new running machines. Machine image formats
+# change for each platform. Some examples include AMIs for EC2, VMDK/VMX files for VMware, OVF
+# exports for VirtualBox, etc.
+#
+# For more details, please visit:
+#   https://packer.io/
+#
+# NOTE: Script should be run with 'root' privilege.
+#---------------------------------------------------------------------------------------------------
+
+# install hashicorp packer. ------------------------------------------------------------------------
+packer_release="1.6.5"
+packer_binary="packer_${packer_release}_linux_amd64.zip"
+packer_sha256="a49f6408a50c220fe3f1a6192ea21134e2e8f31092c507614cd27ad4f913234b"
+
+# create local bin directory (if needed).
+mkdir -p /usr/local/bin
+cd /usr/local/bin
+
+# download packer binary from hashicorp.com.
+rm -f ${packer_binary}
+wget --no-verbose https://releases.hashicorp.com/packer/${packer_release}/${packer_binary}
+
+# verify the downloaded binary.
+echo "${packer_sha256} ${packer_binary}" | sha256sum --check
+# packer_${packer_release}_linux_amd64.zip: OK
+
+# extract packer binary.
+rm -f packer
+unzip ${packer_binary}
+chmod 755 packer
+rm -f ${packer_binary}
+
+# verify installation. -----------------------------------------------------------------------------
+# set packer environment variables.
+PATH=/usr/local/bin:$PATH
+export PATH
+
+# verify packer version.
+packer --version
