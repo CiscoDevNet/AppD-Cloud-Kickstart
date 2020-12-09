d7b058039fb07942ed9882e220f07efaa7e0f029
Ed Barberis <ed.barberis@gmail.com>
2020-12-09 15:06:59 -0500

    Upgraded AppDynamics Enterprise Console to 20.11.3.23827.

--- provisioners/scripts/centos/install_centos7_appdynamics_enterprise_console.sh
+++ provisioners/scripts/centos/install_centos7_appdynamics_enterprise_console.sh
@@ -30,8 +30,8 @@ set -x  # turn command display back ON.
 # appd platform install parameters.
 appd_home="${appd_home:-/opt/appdynamics}"
 appd_platform_home="${appd_platform_home:-platform}"
-appd_platform_release="${appd_platform_release:-20.11.2.23819}"
-appd_platform_sha256="${appd_platform_sha256:-b542f8b24084dee43ab7a2ac7131bcff81aab359213b79325e218b758c82f85d}"
+appd_platform_release="${appd_platform_release:-20.11.3.23827}"
+appd_platform_sha256="${appd_platform_sha256:-cb14cba8a9ca3ba1f8cfe5c72984a9b4d94d67293effaaaa0c836ce32aa78967}"
 appd_platform_user_name="${appd_platform_user_name:-centos}"
 appd_platform_user_group="${appd_platform_user_group:-centos}"
 set +x  # temporarily turn command display OFF.
@@ -67,9 +67,9 @@ Usage:
   [OPTIONAL] appdynamics platform install parameters [w/ defaults].
     [root]# export appd_home="/opt/appdynamics"                         # [optional] appd home (defaults to '/opt/appdynamics').
     [root]# export appd_platform_home="platform"                        # [optional] platform home folder (defaults to 'platform').
-    [root]# export appd_platform_release="20.11.2.23819"                # [optional] platform release (defaults to '20.11.2.23819').
+    [root]# export appd_platform_release="20.11.3.23827"                # [optional] platform release (defaults to '20.11.3.23827').
                                                                         # [optional] platform sha-256 checksum (defaults to published value).
-    [root]# export appd_platform_sha256="b542f8b24084dee43ab7a2ac7131bcff81aab359213b79325e218b758c82f85d"
+    [root]# export appd_platform_sha256="cb14cba8a9ca3ba1f8cfe5c72984a9b4d94d67293effaaaa0c836ce32aa78967"
     [root]# export appd_platform_user_name="centos"                     # [optional] platform user name (defaults to 'centos').
     [root]# export appd_platform_user_group="centos"                    # [optional] platform group (defaults to 'centos').
     [root]# export appd_platform_admin_username="admin"                 # [optional] platform admin user name (defaults to user 'admin').
