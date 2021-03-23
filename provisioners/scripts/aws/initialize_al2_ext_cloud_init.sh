a64dcf03f8fcffcbe6b834db879acc47c8f1ed15
Ed Barberis <ed.barberis@gmail.com>
2021-03-23 17:58:41 -0400

    Upgraded AppDynamics Java Agent JDK8+ to 21.3.0.32281.

--- provisioners/scripts/aws/initialize_al2_ext_cloud_init.sh
+++ provisioners/scripts/aws/initialize_al2_ext_cloud_init.sh
@@ -25,7 +25,7 @@ appd_controller_account_access_key="${appd_controller_account_access_key:-abcdef
 
 # [OPTIONAL] override appdynamics java agent config parameters.
 appd_java_agent_home="${appd_java_agent_home:-appagent}"
-appd_java_agent_release="${appd_java_agent_release:-21.2.0.31849}"
+appd_java_agent_release="${appd_java_agent_release:-21.3.0.32281}"
 
 # [OPTIONAL] override appdynamics machine agent config parameters.
 appd_machine_agent_home="${appd_machine_agent_home:-machine-agent}"
