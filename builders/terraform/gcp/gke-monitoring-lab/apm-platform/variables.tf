489164d627b9e8627bff1481ff0008369e277450
Ed Barberis <ed.barberis@gmail.com>
2021-03-29 14:45:28 -0400

    Upgraded Terraform AWS provider to 3.34.

--- builders/terraform/gcp/gke-monitoring-lab/apm-platform/variables.tf
+++ builders/terraform/gcp/gke-monitoring-lab/apm-platform/variables.tf
@@ -62,7 +62,7 @@ variable "gcp_source_image_family" {
 variable "gcp_source_image" {
   description = "The source disk image."
   type        = string
-  default     = "apm-platform-centos79-2021-03-25"
+  default     = "apm-platform-centos79-2021-03-29"
 # default     = "centos-7-v20200910"
 }
 
