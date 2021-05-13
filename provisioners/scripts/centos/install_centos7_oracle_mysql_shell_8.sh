31186f10fdced5228e4532bc32bca238b21215b2
Ed Barberis <ed.barberis@gmail.com>
2021-05-13 14:39:38 -0400

    Upgraded MySQL Shell to version 8.0.25.

--- provisioners/scripts/centos/install_centos7_oracle_mysql_shell_8.sh
+++ provisioners/scripts/centos/install_centos7_oracle_mysql_shell_8.sh
@@ -9,9 +9,9 @@ mkdir -p ${kickstart_home}/provisioners/scripts/centos
 cd ${kickstart_home}/provisioners/scripts/centos
 
 # install mysql shell. -----------------------------------------------------------------------------
-mysqlsh_release="8.0.24-1"
+mysqlsh_release="8.0.25-1"
 mysqlsh_binary="mysql-shell-${mysqlsh_release}.el7.x86_64.rpm"
-mysqlsh_checksum="49085daa1a904f0e5629df4e51255fc5"
+mysqlsh_checksum="dadd571c7397289edcef6649384f3599"
 
 # download mysql shell repository.
 rm -f ${mysqlsh_binary}
