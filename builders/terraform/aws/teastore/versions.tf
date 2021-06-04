f233ba33ed03f0a9e983a9cc9c7631ed9886baae
Ed Barberis <ed.barberis@gmail.com>
2021-06-04 14:24:33 -0400

    Upgraded Terraform provider support for AWS 3.44 and Azure 2.62.

--- builders/terraform/aws/teastore/versions.tf
+++ builders/terraform/aws/teastore/versions.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = ">= 3.43"
+      version = ">= 3.44"
     }
 
     local = {
--- builders/terraform/azure/aks-monitoring-lab/apm-platform/versions.tf
+++ builders/terraform/azure/aks-monitoring-lab/apm-platform/versions.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = ">= 2.61.0"
+      version = ">= 2.62.0"
     }
 
     local = {
