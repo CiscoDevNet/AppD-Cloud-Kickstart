e3cdfb1400d87c065efd5ab73f93f114acd1a488
Ed Barberis <ed.barberis@gmail.com>
2021-08-26 10:16:26 -0400

    Upgraded AppDynamics Cluster Agent to 21.7.0.2080.

--- applications/aws/AD-Capital-Kube/create_eks_cluster.sh
+++ applications/aws/AD-Capital-Kube/create_eks_cluster.sh
@@ -48,7 +48,7 @@ Usage:
     [centos]$ export appd_aws_eks_zones="us-east-1a,us-east-1b,us-east-1c"      # [optional] aws availability zones.
     [centos]$ export appd_aws_eks_version="1.20"                                # [optional] kubernetes version (defaults to '1.20').
                                                                                 #            valid versions:
-                                                                                #              '1.16', '1.17', '1.18', '1.19', '1.20'
+                                                                                #              '1.16', '1.17', '1.18', '1.19', '1.20', '1.21'
     [centos]$ export appd_aws_eks_name="AD-Capital-Lab-User-01"                 # [optional] eks cluster name (defaults to 'AD-Capital-${appd_aws_eks_user_name}')
     [centos]$ export appd_aws_eks_environment="Lab-Env-Lab-User-01"             # [optional] eks environment decription (defaults to 'Lab-Env-${appd_aws_eks_user_name}')
     [centos]$ export appd_aws_eks_nodes="2"                                     # [optional] number of eks worker nodes (defaults to '2')
@@ -77,7 +77,7 @@ fi
 
 if [ -n "$appd_aws_eks_version" ]; then
   case $appd_aws_eks_version in
-      1.16|1.17|1.18|1.19|1.20)
+      1.16|1.17|1.18|1.19|1.20|1.21)
         ;;
       *)
         echo "Error: invalid 'appd_aws_eks_version'."
