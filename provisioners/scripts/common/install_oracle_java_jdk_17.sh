d9feee29a2810ff5d498d7786b21343b85ed87f2
Ed Barberis <ed.barberis@gmail.com>
2021-09-22 14:54:01 -0400

    Added support for Amazon Corretto JDK 17 and Oracle Java SE JDK 17.

---
+++ provisioners/scripts/common/install_oracle_java_jdk_17.sh
@@ -0,0 +1,36 @@
+#!/bin/sh -eux
+# install java se 17 development kit by oracle.
+
+# install java se 17 development kit. --------------------------------------------------------------
+jdk_home="jdk17"
+jdk_build="17"
+jdk_folder="jdk-${jdk_build}"
+jdk_binary="${jdk_folder}_linux-x64_bin.tar.gz"
+jdk_sha256="106e3fc31b9fe4d0fd76c6c92b1931bdd1b0ac4d788747951de0f92cc73b1ffe"
+
+# create java home parent folder.
+mkdir -p /usr/local/java
+cd /usr/local/java
+
+# download jdk 17 binary from oracle otn.
+wget --no-verbose https://download.oracle.com/java/${jdk_build}/latest/${jdk_binary}
+
+# verify the downloaded binary.
+echo "${jdk_sha256} ${jdk_binary}" | sha256sum --check
+# ${jdk_folder}_linux-x64_bin.tar.gz: OK
+
+# extract jdk 17 binary and create softlink to 'jdk17'.
+rm -f ${jdk_home}
+tar -zxvf ${jdk_binary} --no-same-owner --no-overwrite-dir
+chown -R root:root ./${jdk_folder}
+ln -s ${jdk_folder} ${jdk_home}
+rm -f ${jdk_binary}
+
+# set jdk 17 home environment variables.
+JAVA_HOME=/usr/local/java/${jdk_home}
+export JAVA_HOME
+PATH=${JAVA_HOME}/bin:$PATH
+export PATH
+
+# verify installation.
+java --version
