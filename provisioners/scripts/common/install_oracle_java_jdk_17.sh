a8a6f14f25c04971550f4803688c9ca1742bda63
Ed Barberis <ed.barberis@gmail.com>
2021-10-22 18:37:54 -0400

    Upgraded support for Oracle Java SE JDK 17.0.1.

--- provisioners/scripts/common/install_oracle_java_jdk_17.sh
+++ provisioners/scripts/common/install_oracle_java_jdk_17.sh
@@ -3,17 +3,17 @@
 
 # install java se 17 development kit. --------------------------------------------------------------
 jdk_home="jdk17"
-jdk_build="17"
+jdk_build="17.0.1"
 jdk_folder="jdk-${jdk_build}"
 jdk_binary="${jdk_folder}_linux-x64_bin.tar.gz"
-jdk_sha256="106e3fc31b9fe4d0fd76c6c92b1931bdd1b0ac4d788747951de0f92cc73b1ffe"
+jdk_sha256="6f25bcb94d3e22fb52a4632c74e03b403834e81b68701ab7ecd900fb9cd89f43"
 
 # create java home parent folder.
 mkdir -p /usr/local/java
 cd /usr/local/java
 
 # download jdk 17 binary from oracle otn.
-wget --no-verbose https://download.oracle.com/java/${jdk_build}/latest/${jdk_binary}
+wget --no-verbose https://download.oracle.com/java/${jdk_build:0:2}/archive/${jdk_binary}
 
 # verify the downloaded binary.
 echo "${jdk_sha256} ${jdk_binary}" | sha256sum --check
@@ -21,6 +21,7 @@ echo "${jdk_sha256} ${jdk_binary}" | sha256sum --check
 
 # extract jdk 17 binary and create softlink to 'jdk17'.
 rm -f ${jdk_home}
+rm -rf ${jdk_folder}
 tar -zxvf ${jdk_binary} --no-same-owner --no-overwrite-dir
 chown -R root:root ./${jdk_folder}
 ln -s ${jdk_folder} ${jdk_home}
