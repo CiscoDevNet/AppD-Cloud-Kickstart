a8e5ad31d2548ed93f004b83888a5f7ef08f8816
Ed Barberis <ed.barberis@gmail.com>
2022-01-10 10:56:54 -0500

    Upgraded AppDynamics Machine Agent to 21.12.5.3224.

--- provisioners/scripts/centos/install_centos7_appdynamics_machine_agent.sh
+++ provisioners/scripts/centos/install_centos7_appdynamics_machine_agent.sh
@@ -27,8 +27,8 @@ appd_controller_root_password="${appd_controller_root_password:-welcome1}"
 set -x  # turn command display back ON.
 appd_machine_agent_home="${appd_machine_agent_home:-machine-agent}"
 appd_machine_agent_user="${appd_machine_agent_user:-centos}"
-appd_machine_agent_release="${appd_machine_agent_release:-21.12.4.3218}"
-appd_machine_agent_sha256="${appd_machine_agent_sha256:-5c3ccfaef3896e8d5bfcf9e6987d557e0a364d2147b9276c804e67355ffc3940}"
+appd_machine_agent_release="${appd_machine_agent_release:-21.12.5.3224}"
+appd_machine_agent_sha256="${appd_machine_agent_sha256:-2ffb2f801e7b1b1c2da439e7201b63af357139cfef10145c5464830c04a94868}"
 
 # [OPTIONAL] appdynamics machine agent config parameters [w/ defaults].
 appd_machine_agent_config="${appd_machine_agent_config:-false}"
@@ -65,9 +65,9 @@ Usage:
     [root]# export appd_controller_root_password="welcome1"             # [optional] controller root password (defaults to 'welcome1').
     [root]# export appd_machine_agent_home="machine-agent"              # [optional] machine agent home folder (defaults to 'machine-agent').
     [root]# export appd_machine_agent_user="centos"                     # [optional] machine agent user name (defaults to user 'centos').
-    [root]# export appd_machine_agent_release="21.12.4.3218"            # [optional] machine agent release (defaults to '21.12.4.3218').
+    [root]# export appd_machine_agent_release="21.12.5.3224"            # [optional] machine agent release (defaults to '21.12.5.3224').
                                                                         # [optional] machine agent sha-256 checksum (defaults to published value).
-    [root]# export appd_machine_agent_sha256="5c3ccfaef3896e8d5bfcf9e6987d557e0a364d2147b9276c804e67355ffc3940"
+    [root]# export appd_machine_agent_sha256="2ffb2f801e7b1b1c2da439e7201b63af357139cfef10145c5464830c04a94868"
 
   [OPTIONAL] appdynamics machine agent config parameters [w/ defaults].
     [root]# export appd_machine_agent_config="true"                     # [optional] configure appd machine agent? [boolean] (defaults to 'false').
