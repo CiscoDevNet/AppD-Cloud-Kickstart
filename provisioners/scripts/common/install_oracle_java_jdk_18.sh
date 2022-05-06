05c13bf50581df3efc4224a6bc2f1b02ad99e654
Ed Barberis <ed.barberis@gmail.com>
2022-05-06 15:00:44 -0400

    Upgraded support for Oracle Java SE JDK 17.0.3.1 and 18.0.1.1.

--- provisioners/scripts/common/install_oracle_java_jdk_18.sh
+++ provisioners/scripts/common/install_oracle_java_jdk_18.sh
@@ -1,25 +1,25 @@
 #!/bin/sh -eux
-# install java se 17 development kit by oracle.
+# install java se 18 development kit by oracle.
 
-# install java se 17 development kit. --------------------------------------------------------------
-jdk_home="jdk17"
-jdk_build="17.0.3.1"
+# install java se 18 development kit. --------------------------------------------------------------
+jdk_home="jdk18"
+jdk_build="18.0.1.1"
 jdk_folder="jdk-${jdk_build}"
 jdk_binary="${jdk_folder}_linux-x64_bin.tar.gz"
-jdk_sha256="11b4465229b77fa84416a14b5e7023b6d2cf03cda5eb1557d57aea0247fff643"
+jdk_sha256="dbabd3f726775a63329254b001b4190c082206e38323950b2db478606f1d65fa"
 
 # create java home parent folder.
 mkdir -p /usr/local/java
 cd /usr/local/java
 
-# download jdk 17 binary from oracle otn.
+# download jdk 18 binary from oracle otn.
 wget --no-verbose https://download.oracle.com/java/${jdk_build:0:2}/archive/${jdk_binary}
 
 # verify the downloaded binary.
 echo "${jdk_sha256} ${jdk_binary}" | sha256sum --check
 # ${jdk_folder}_linux-x64_bin.tar.gz: OK
 
-# extract jdk 17 binary and create softlink to 'jdk17'.
+# extract jdk 18 binary and create softlink to 'jdk18'.
 rm -f ${jdk_home}
 rm -rf ${jdk_folder}
 tar -zxvf ${jdk_binary} --no-same-owner --no-overwrite-dir
@@ -27,7 +27,7 @@ chown -R root:root ./${jdk_folder}
 ln -s ${jdk_folder} ${jdk_home}
 rm -f ${jdk_binary}
 
-# set jdk 17 home environment variables.
+# set jdk 18 home environment variables.
 JAVA_HOME=/usr/local/java/${jdk_home}
 export JAVA_HOME
 PATH=${JAVA_HOME}/bin:$PATH
