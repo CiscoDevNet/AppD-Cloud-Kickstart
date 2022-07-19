e3b6dc5a1a8d3f7e3e835131a3987d722964d6d2
Ed Barberis <ed.barberis@gmail.com>
2022-07-19 10:00:08 -0400

    Upgraded Terraform to 1.2.5; providers for AWS 4.22, Azure 3.14, and GCP 4.29.

--- builders/terraform/gcp/gke-monitoring-lab/apm-platform/modules/instance_template/versions.tf
+++ builders/terraform/gcp/gke-monitoring-lab/apm-platform/modules/instance_template/versions.tf
@@ -17,7 +17,7 @@
 terraform {
   required_version = ">= 1.2.5"
   required_providers {
-    google = ">= 4.28, < 5.0"
+    google = ">= 4.29, < 5.0"
   }
   provider_meta "google" {
     module_name = "blueprints/terraform/terraform-google-vm:instance_template/v7.8.0"
