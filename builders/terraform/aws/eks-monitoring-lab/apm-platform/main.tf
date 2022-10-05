4420853cf6ea7687ae9d6f794f18ca7da2b3a10a
Ed Barberis <ed.barberis@gmail.com>
2022-10-05 10:46:11 -0400

    Updated AppDynamics resource tagging schema for AWS per new requirements.

--- builders/terraform/aws/eks-monitoring-lab/apm-platform/main.tf
+++ builders/terraform/aws/eks-monitoring-lab/apm-platform/main.tf
@@ -1,21 +1,66 @@
 # Providers ----------------------------------------------------------------------------------------
 provider "aws" {
-  region  = var.aws_region
+  region = var.aws_region
 }
 
 # Locals -------------------------------------------------------------------------------------------
 locals {
+  # format current date for convenience.
   current_date = formatdate("YYYY-MM-DD", timestamp())
+
+  # create range of formatted lab numbers.
   lab_for_each = toset([ for i in range(var.lab_start_number, var.lab_count + var.lab_start_number) : format("%02d", i) ])
+
+  # create vm ssh ingress cidr block list without duplicates.
+  vm_ssh_ingress_cidr_blocks = join(",", distinct(tolist([var.aws_ssh_ingress_cidr_blocks, var.cisco_ssh_ingress_cidr_blocks, var.aws_cloud9_ssh_ingress_cidr_blocks, var.aws_ec2_instance_connect_ssh_ingress_cidr_blocks])))
+
+  # define resource tagging here to ensure standardized naming conventions.
+  # lpad tag names for aws resources.
+  lpad_resource_tags = {
+    EnvironmentHome = var.resource_environment_home_tag
+    Owner           = var.resource_owner_tag
+    Event           = var.resource_event_tag
+    Project         = var.resource_project_tag
+    Date            = local.current_date
+  }
+
+  # appdynamics tag names for aws resources.
+  appd_resource_tags = {
+    ResourceOwner         = var.resource_owner_email_tag
+    CiscoMailAlias        = var.resource_owner_email_tag
+    JIRAProject           = "NA"
+    DataClassification    = "Cisco Public"
+    JIRACreation          = "NA"
+    SecurityReview        = "NA"
+    Exception             = "NA"
+    Environment           = "NonProd"
+    DeploymentEnvironment = "NonProd"
+    DataTaxonomy          = "Cisco Operations Data"
+    CreatedBy             = data.aws_caller_identity.current.arn
+    IntendedPublic        = "True"
+    ContainsPII           = "False"
+    Service               = "CloudKickstartLab"
+    ApplicationName       = var.resource_project_tag
+    CostCenter            = var.resource_cost_center_tag
+  }
+
+  # merge 'lpad_resource_tags' and 'appd_resource_tags' into one set.
+  resource_tags = merge(local.lpad_resource_tags, local.appd_resource_tags)
 }
 
 # Data Sources -------------------------------------------------------------------------------------
+data "aws_caller_identity" "current" {
+}
+
+data "aws_availability_zones" "available" {
+}
+
 data "aws_ami" "apm_platform_al2" {
   most_recent = true
   owners      = ["self"]
 
   filter {
-    name = "name"
+    name   = "name"
     values = [var.aws_ec2_source_ami_filter]
   }
 }
@@ -23,20 +68,20 @@ data "aws_ami" "apm_platform_al2" {
 # Modules ------------------------------------------------------------------------------------------
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = ">= 3.14"
+  version = ">= 3.16"
 
   name = "VPC-${var.resource_name_prefix}-${local.current_date}"
-  cidr = var.aws_vpc_cidr
+  cidr = var.aws_vpc_cidr_block
 
-  azs             = var.aws_availability_zones
-  public_subnets  = var.aws_vpc_public_subnets
+  azs            = data.aws_availability_zones.available.names
+  public_subnets = var.aws_vpc_public_subnets
 
   enable_nat_gateway   = true
   enable_vpn_gateway   = false
   enable_dns_hostnames = true
   enable_dns_support   = true
 
-  tags = var.resource_tags
+  tags = local.resource_tags
 }
 
 module "security_group" {
@@ -46,10 +91,10 @@ module "security_group" {
   name        = "SG-${var.resource_name_prefix}-${local.current_date}"
   description = "Security group for example usage with EC2 instance"
   vpc_id      = module.vpc.vpc_id
-  tags        = var.resource_tags
+  tags        = local.resource_tags
 
   ingress_cidr_blocks               = ["0.0.0.0/0"]
-  ingress_rules                     = ["http-80-tcp", "http-8080-tcp", "https-443-tcp", "mysql-tcp"]
+  ingress_rules                     = ["http-80-tcp", "http-8080-tcp", "https-443-tcp"]
   egress_rules                      = ["all-all"]
   ingress_with_self                 = [{rule = "all-all"}]
   computed_ingress_with_cidr_blocks = [
@@ -58,7 +103,7 @@ module "security_group" {
       to_port     = 22
       protocol    = "tcp"
       description = "SSH port"
-      cidr_blocks = var.aws_ssh_ingress_cidr_blocks
+      cidr_blocks = local.vm_ssh_ingress_cidr_blocks
     },
     {
       from_port   = 9191
@@ -110,7 +155,7 @@ module "apm_platform_vm" {
   instance_type        = var.aws_ec2_instance_type
   iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.id
   key_name             = var.aws_ec2_ssh_pub_key_name
-  tags                 = var.resource_tags
+  tags                 = local.resource_tags
 
   capacity_reservation_specification = {
     capacity_reservation_preference = "none"
@@ -134,13 +179,17 @@ module "apm_platform_vm" {
 resource "aws_iam_role" "ec2_access_role" {
   name               = "EC2-Access-Role-${var.resource_name_prefix}-${local.current_date}"
   assume_role_policy = file("${path.module}/policies/ec2-assume-role-policy.json")
-  tags               = var.resource_tags
+  tags               = local.resource_tags
 }
 
 resource "aws_iam_role_policy" "ec2_access_policy" {
   name   = "EC2-Access-Policy-${var.resource_name_prefix}-${local.current_date}"
   role   = aws_iam_role.ec2_access_role.id
-  policy = file("${path.module}/policies/ec2-access-policy.json")
+  policy = templatefile("${path.module}/policies/ec2-access-policy-template.json", {
+    aws_region_name   = var.aws_region
+    aws_account_id    = data.aws_caller_identity.current.account_id
+    aws_ec2_user_name = var.aws_ec2_user_name
+  })
 }
 
 resource "aws_iam_instance_profile" "ec2_instance_profile" {
