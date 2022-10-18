0b52f9abeebba8c6f811c1226c66f2a0be7569e1
Ed Barberis <ed.barberis@gmail.com>
2022-10-18 09:52:18 -0400

    Upgraded Terraform to 1.3.2; providers for AWS 4.35, Azure 3.27, and GCP 4.41.

--- builders/terraform/aws/teastore/versions.tf
+++ builders/terraform/aws/teastore/versions.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = ">= 4.34"
+      version = ">= 4.35"
     }
 
     local = {
--- builders/terraform/gcp/teastore/versions.tf
+++ builders/terraform/gcp/teastore/versions.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     google = {
       source = "hashicorp/google"
-      version = ">= 4.40, < 5.0"
+      version = ">= 4.41, < 5.0"
     }
 
     null = {
