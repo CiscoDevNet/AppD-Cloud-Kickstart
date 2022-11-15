a9752bd61152a28534027977dfaa4ad88c77ff7e
Ed Barberis <ed.barberis@gmail.com>
2022-11-15 14:52:28 -0500

    Updated AWS AMI search filters to use latest CentOS 7 base image.

--- provisioners/scripts/utils/get_aws_al2_ami_ids_by_region.sh
+++ provisioners/scripts/utils/get_aws_al2_ami_ids_by_region.sh
@@ -15,15 +15,15 @@ curdate=$(date +"%d %b %Y")
 echo "# AWS AL2 AMI Image IDs as of: ${curdate}."
 
 # loop for each aws region. ------------------------------------------------------------------------
-# initialize aws regions for north america and europe.
-aws_region_ids_array=( "ap-southeast-2" "ca-central-1" "eu-central-1" "eu-west-1" "eu-west-2" "eu-west-3" "sa-east-1" "us-east-1" "us-east-2" "us-west-1" "us-west-2" )
-aws_region_names_array=( "Sydney" "Central" "Frankfurt" "Ireland" "London" "Paris" "São Paulo" "N. Virginia" "Ohio" "N. California" "Oregon" )
+# initialize aws regions for asia pacific, north america, and europe.
+aws_region_ids_array=( "ap-south-1" "ap-southeast-1" "ap-southeast-2" "ca-central-1" "eu-central-1" "eu-west-1" "eu-west-2" "eu-west-3" "sa-east-1" "us-east-1" "us-east-2" "us-west-1" "us-west-2" )
+aws_region_names_array=( "Mumbai" "Singapore" "Sydney" "Central" "Frankfurt" "Ireland" "London" "Paris" "São Paulo" "N. Virginia" "Ohio" "N. California" "Oregon" )
 
 # initialize array index.
 ii=0
 for aws_region in "${aws_region_ids_array[@]}"; do
   # retrieve ami source image id.
-  aws_ami_source=$(aws --region=${aws_region} ec2 describe-images --owners amazon --filters 'Name=name,Values=amzn2-ami-hvm-2.0.*-x86_64-gp2' 'Name=state,Values=available' --output json | jq -r '.Images | sort_by(.CreationDate) | last(.[]).ImageId')
+  aws_ami_source=$(aws --region=${aws_region} ec2 describe-images --owners amazon --filters 'Name=name,Values=*amzn2-ami-hvm-2.0.*-x86_64-gp2' 'Name=state,Values=available' --output json | jq -r '.Images | sort_by(.CreationDate) | last(.[]).ImageId')
   echo "#aws_ami_source=\"${aws_ami_source}\"     # Amazon Linux 2 AMI (HVM), SSD Volume Type: ${aws_region} [${aws_region_names_array[$ii]}]"
 
   # increment array index.
@@ -33,16 +33,18 @@ done
 echo "#export aws_ami_source"
 
 # output will look like the following: -------------------------------------------------------------
-# AWS AL2 AMI Image IDs as of: 14 Jan 2020.
-#aws_ami_source="ami-0b8b10b5bf11f3a22"     # Amazon Linux 2 AMI (HVM), SSD Volume Type: ap-southeast-2 [Sydney]
-#aws_ami_source="ami-0a269ca7cc3e3beff"     # Amazon Linux 2 AMI (HVM), SSD Volume Type: ca-central-1 [Central]
-#aws_ami_source="ami-07cda0db070313c52"     # Amazon Linux 2 AMI (HVM), SSD Volume Type: eu-central-1 [Frankfurt]
-#aws_ami_source="ami-0713f98de93617bb4"     # Amazon Linux 2 AMI (HVM), SSD Volume Type: eu-west-1 [Ireland]
-#aws_ami_source="ami-0089b31e09ac3fffc"     # Amazon Linux 2 AMI (HVM), SSD Volume Type: eu-west-2 [London]
-#aws_ami_source="ami-007fae589fdf6e955"     # Amazon Linux 2 AMI (HVM), SSD Volume Type: eu-west-3 [Paris]
-#aws_ami_source="ami-09de7b4017733e2af"     # Amazon Linux 2 AMI (HVM), SSD Volume Type: sa-east-1 [São Paulo]
-#aws_ami_source="ami-062f7200baf2fa504"     # Amazon Linux 2 AMI (HVM), SSD Volume Type: us-east-1 [N. Virginia]
-#aws_ami_source="ami-02ccb28830b645a41"     # Amazon Linux 2 AMI (HVM), SSD Volume Type: us-east-2 [Ohio]
-#aws_ami_source="ami-03caa3f860895f82e"     # Amazon Linux 2 AMI (HVM), SSD Volume Type: us-west-1 [N. California]
-#aws_ami_source="ami-04590e7389a6e577c"     # Amazon Linux 2 AMI (HVM), SSD Volume Type: us-west-2 [Oregon]
+# AWS AL2 AMI Image IDs as of: 14 Nov 2022.
+#aws_ami_source="ami-01f703c132f2b1a20"     # Amazon Linux 2 AMI (HVM), SSD Volume Type: ap-south-1 [Mumbai]
+#aws_ami_source="ami-0d46390da1dfb8792"     # Amazon Linux 2 AMI (HVM), SSD Volume Type: ap-southeast-1 [Singapore]
+#aws_ami_source="ami-02b86aca723e9967e"     # Amazon Linux 2 AMI (HVM), SSD Volume Type: ap-southeast-2 [Sydney]
+#aws_ami_source="ami-05926d68079b85f1c"     # Amazon Linux 2 AMI (HVM), SSD Volume Type: ca-central-1 [Central]
+#aws_ami_source="ami-0b920b0594b5288fb"     # Amazon Linux 2 AMI (HVM), SSD Volume Type: eu-central-1 [Frankfurt]
+#aws_ami_source="ami-0dcc20d3a15abf9bb"     # Amazon Linux 2 AMI (HVM), SSD Volume Type: eu-west-1 [Ireland]
+#aws_ami_source="ami-060c0d1361bbd1bd7"     # Amazon Linux 2 AMI (HVM), SSD Volume Type: eu-west-2 [London]
+#aws_ami_source="ami-0a53f4992623d6f03"     # Amazon Linux 2 AMI (HVM), SSD Volume Type: eu-west-3 [Paris]
+#aws_ami_source="ami-02e0498e7e54ef3c9"     # Amazon Linux 2 AMI (HVM), SSD Volume Type: sa-east-1 [São Paulo]
+#aws_ami_source="ami-0c4e4b4eb2e11d1d4"     # Amazon Linux 2 AMI (HVM), SSD Volume Type: us-east-1 [N. Virginia]
+#aws_ami_source="ami-07693758d0ebc2111"     # Amazon Linux 2 AMI (HVM), SSD Volume Type: us-east-2 [Ohio]
+#aws_ami_source="ami-061352bb71c4724b2"     # Amazon Linux 2 AMI (HVM), SSD Volume Type: us-west-1 [N. California]
+#aws_ami_source="ami-0f9f005c313373218"     # Amazon Linux 2 AMI (HVM), SSD Volume Type: us-west-2 [Oregon]
 #export aws_ami_source
--- provisioners/scripts/utils/get_aws_centos7_ami_ids_by_region.sh
+++ provisioners/scripts/utils/get_aws_centos7_ami_ids_by_region.sh
@@ -12,19 +12,20 @@ fi
 # print comment line with date. --------------------------------------------------------------------
 # get current date.
 curdate=$(date +"%d %b %Y")
-echo "# CentOS 7.7 AMI Image IDs as of: ${curdate}."
+echo "# CentOS 7.9 AMI Image IDs as of: ${curdate}."
 
 # loop for each aws region. ------------------------------------------------------------------------
-# initialize aws regions for north america and europe.
-aws_region_ids_array=( "ap-southeast-2" "ca-central-1" "eu-central-1" "eu-west-1" "eu-west-2" "eu-west-3" "sa-east-1" "us-east-1" "us-east-2" "us-west-1" "us-west-2" )
-aws_region_names_array=( "Sydney" "Central" "Frankfurt" "Ireland" "London" "Paris" "São Paulo" "N. Virginia" "Ohio" "N. California" "Oregon" )
+# initialize aws regions for asia pacific, north america, and europe.
+aws_region_ids_array=( "ap-south-1" "ap-southeast-1" "ap-southeast-2" "ca-central-1" "eu-central-1" "eu-west-1" "eu-west-2" "eu-west-3" "sa-east-1" "us-east-1" "us-east-2" "us-west-1" "us-west-2" )
+aws_region_names_array=( "Mumbai" "Singapore" "Sydney" "Central" "Frankfurt" "Ireland" "London" "Paris" "São Paulo" "N. Virginia" "Ohio" "N. California" "Oregon" )
 
 # initialize array index.
 ii=0
 for aws_region in "${aws_region_ids_array[@]}"; do
   # retrieve ami source image id.
-  aws_ami_source=$(aws --region=${aws_region} ec2 describe-images --owners aws-marketplace --filters 'Name=product-code,Values=aw0evgkw8e5c1q413zgy5pjce' 'Name=state,Values=available' --output json | jq -r '.Images | sort_by(.CreationDate) | last(.[]).ImageId')
-  echo "#aws_ami_source=\"${aws_ami_source}\"     # CentOS 7.7 AMI (HVM), SSD Volume Type: ${aws_region} [${aws_region_names_array[$ii]}]"
+  aws_ami_source=$(aws --region=${aws_region} ec2 describe-images --owners aws-marketplace --filters 'Name=name,Values=CentOS-7-2111*' 'Name=state,Values=available' --output json | jq -r '.Images | sort_by(.CreationDate) | last(.[]).ImageId')
+# aws_ami_source=$(aws --region=${aws_region} ec2 describe-images --owners aws-marketplace --filters 'Name=product-code,Values=aw0evgkw8e5c1q413zgy5pjce' 'Name=state,Values=available' --output json | jq -r '.Images | sort_by(.CreationDate) | last(.[]).ImageId')
+  echo "#aws_ami_source=\"${aws_ami_source}\"     # CentOS 7.9 AMI (HVM), SSD Volume Type: ${aws_region} [${aws_region_names_array[$ii]}]"
 
   # increment array index.
   ii=$(($ii + 1))
@@ -33,16 +34,18 @@ done
 echo "#export aws_ami_source"
 
 # output will look like the following: -------------------------------------------------------------
-# CentOS 7.7 AMI Image IDs as of: 14 Jan 2020.
-#aws_ami_source="ami-08bd00d7713a39e7d"     # CentOS 7.7 AMI (HVM), SSD Volume Type: ap-southeast-2 [Sydney]
-#aws_ami_source="ami-033e6106180a626d0"     # CentOS 7.7 AMI (HVM), SSD Volume Type: ca-central-1 [Central]
-#aws_ami_source="ami-04cf43aca3e6f3de3"     # CentOS 7.7 AMI (HVM), SSD Volume Type: eu-central-1 [Frankfurt]
-#aws_ami_source="ami-0ff760d16d9497662"     # CentOS 7.7 AMI (HVM), SSD Volume Type: eu-west-1 [Ireland]
-#aws_ami_source="ami-0eab3a90fc693af19"     # CentOS 7.7 AMI (HVM), SSD Volume Type: eu-west-2 [London]
-#aws_ami_source="ami-0e1ab783dc9489f34"     # CentOS 7.7 AMI (HVM), SSD Volume Type: eu-west-3 [Paris]
-#aws_ami_source="ami-0b8d86d4bf91850af"     # CentOS 7.7 AMI (HVM), SSD Volume Type: sa-east-1 [São Paulo]
-#aws_ami_source="ami-02eac2c0129f6376b"     # CentOS 7.7 AMI (HVM), SSD Volume Type: us-east-1 [N. Virginia]
-#aws_ami_source="ami-0f2b4fc905b0bd1f1"     # CentOS 7.7 AMI (HVM), SSD Volume Type: us-east-2 [Ohio]
-#aws_ami_source="ami-074e2d6769f445be5"     # CentOS 7.7 AMI (HVM), SSD Volume Type: us-west-1 [N. California]
-#aws_ami_source="ami-01ed306a12b7d1c96"     # CentOS 7.7 AMI (HVM), SSD Volume Type: us-west-2 [Oregon]
+# CentOS 7.9 AMI Image IDs as of: 14 Nov 2022.
+#aws_ami_source="ami-0763cf792771fe1bd"     # CentOS 7.9 AMI (HVM), SSD Volume Type: ap-south-1 [Mumbai]
+#aws_ami_source="ami-00d785f1c099d5a0e"     # CentOS 7.9 AMI (HVM), SSD Volume Type: ap-southeast-1 [Singapore]
+#aws_ami_source="ami-0cf5f53cea16d8cbf"     # CentOS 7.9 AMI (HVM), SSD Volume Type: ap-southeast-2 [Sydney]
+#aws_ami_source="ami-0ca3e32c623d61bdf"     # CentOS 7.9 AMI (HVM), SSD Volume Type: ca-central-1 [Central]
+#aws_ami_source="ami-0b4c74d41ee4bed78"     # CentOS 7.9 AMI (HVM), SSD Volume Type: eu-central-1 [Frankfurt]
+#aws_ami_source="ami-0c1f3a8058fde8814"     # CentOS 7.9 AMI (HVM), SSD Volume Type: eu-west-1 [Ireland]
+#aws_ami_source="ami-036e229aa5fa198ba"     # CentOS 7.9 AMI (HVM), SSD Volume Type: eu-west-2 [London]
+#aws_ami_source="ami-0eb3117f2ccc34ba6"     # CentOS 7.9 AMI (HVM), SSD Volume Type: eu-west-3 [Paris]
+#aws_ami_source="ami-04384c010169ed8d3"     # CentOS 7.9 AMI (HVM), SSD Volume Type: sa-east-1 [São Paulo]
+#aws_ami_source="ami-002070d43b0a4f171"     # CentOS 7.9 AMI (HVM), SSD Volume Type: us-east-1 [N. Virginia]
+#aws_ami_source="ami-05a36e1502605b4aa"     # CentOS 7.9 AMI (HVM), SSD Volume Type: us-east-2 [Ohio]
+#aws_ami_source="ami-0dee0f906cf114191"     # CentOS 7.9 AMI (HVM), SSD Volume Type: us-west-1 [N. California]
+#aws_ami_source="ami-08c191625cfb7ee61"     # CentOS 7.9 AMI (HVM), SSD Volume Type: us-west-2 [Oregon]
 #export aws_ami_source
