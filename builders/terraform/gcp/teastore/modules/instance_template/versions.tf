958405d2e9bf1d6bb215c473393c643bfd17cbff
Ed Barberis <ed.barberis@gmail.com>
2023-03-13 16:07:32 -0400

    Upgraded Terraform to 1.4.0; providers for AWS 4.58, Azure 3.47.0, and GCP 4.57.

--- builders/terraform/gcp/teastore/modules/instance_template/versions.tf
+++ builders/terraform/gcp/teastore/modules/instance_template/versions.tf
@@ -17,7 +17,7 @@
 terraform {
   required_version = ">= 1.4.0"
   required_providers {
-    google = ">= 4.56, < 5.0"
+    google = ">= 4.57, < 5.0"
   }
   provider_meta "google" {
     module_name = "blueprints/terraform/terraform-google-vm:instance_template/v8.0.0"
