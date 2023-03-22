93c69940d86ab0886bd23786dfada5aeee13ca29
Ed Barberis <ed.barberis@gmail.com>
2023-03-22 09:02:43 -0400

    Added support for Oracle Java SE JDK 20.

---
+++ provisioners/scripts/common/install_oracle_java_jdk_20.sh
@@ -0,0 +1,37 @@
+#!/bin/sh -eux
+# install java se 20 development kit by oracle.
+
+# install java se 20 development kit. --------------------------------------------------------------
+jdk_home="jdk20"
+jdk_build="20"
+jdk_folder="jdk-${jdk_build}"
+jdk_binary="${jdk_folder}_linux-x64_bin.tar.gz"
+jdk_sha256="1157bba2126efd115b5133cc078e6bcc52b15583657e980a5197f34a7505222e"
+
+# create java home parent folder.
+mkdir -p /usr/local/java
+cd /usr/local/java
+
+# download jdk 20 binary from oracle otn.
+wget --no-verbose https://download.oracle.com/java/${jdk_build:0:2}/archive/${jdk_binary}
+
+# verify the downloaded binary.
+echo "${jdk_sha256} ${jdk_binary}" | sha256sum --check
+# ${jdk_folder}_linux-x64_bin.tar.gz: OK
+
+# extract jdk 20 binary and create softlink to 'jdk20'.
+rm -f ${jdk_home}
+rm -rf ${jdk_folder}
+tar -zxvf ${jdk_binary} --no-same-owner --no-overwrite-dir
+chown -R root:root ./${jdk_folder}
+ln -s ${jdk_folder} ${jdk_home}
+rm -f ${jdk_binary}
+
+# set jdk 20 home environment variables.
+JAVA_HOME=/usr/local/java/${jdk_home}
+export JAVA_HOME
+PATH=${JAVA_HOME}/bin:$PATH
+export PATH
+
+# verify installation.
+java --version
