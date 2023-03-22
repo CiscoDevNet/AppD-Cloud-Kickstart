c8fc88ad0fa3681e72568f4684882d5bcfcf069f
Ed Barberis <ed.barberis@gmail.com>
2023-03-22 09:02:43 -0400

    Added Docker support to Amazon Linux 2023 LPAD AMI.

---
+++ provisioners/scripts/aws/install_al2023_docker.sh
@@ -0,0 +1,28 @@
+#!/bin/sh -eux
+# install the docker engine on amazon linux 2023.
+
+# install the docker community edition engine. ---------------------------------
+dnf -y install docker
+
+# configure docker. ------------------------------------------------------------
+# enable ip forwarding if not set.
+sysctlfile="/etc/sysctl.conf"
+ipv4cmd="net.ipv4.ip_forward = 1"
+if [ -f "$sysctlfile" ]; then
+  sysctl net.ipv4.ip_forward
+  grep -qF "${ipv4cmd}" ${sysctlfile} || echo "${ipv4cmd}" >> ${sysctlfile}
+  sysctl -p /etc/sysctl.conf
+  sysctl net.ipv4.ip_forward
+fi
+
+# start the docker service and configure it to start at boot time.
+systemctl start docker
+systemctl enable docker
+
+# check that the docker service is running.
+systemctl status docker
+
+# display configuration info and verify version.
+docker info
+docker version
+docker --version
