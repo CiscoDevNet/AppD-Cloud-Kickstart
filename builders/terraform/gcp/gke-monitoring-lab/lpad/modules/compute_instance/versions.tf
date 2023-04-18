cfa4dedffab939eee512cfd5a4cbceef1887ed3c
Ed Barberis <ed.barberis@gmail.com>
2023-04-18 16:22:05 -0400

    Upgraded Terraform to 1.4.5; providers for AWS 4.63, Azure 3.52.0, and GCP 4.62.

--- builders/terraform/gcp/gke-monitoring-lab/lpad/modules/compute_instance/versions.tf
+++ builders/terraform/gcp/gke-monitoring-lab/lpad/modules/compute_instance/versions.tf
@@ -17,7 +17,7 @@
 terraform {
   required_version = ">= 1.4.5"
   required_providers {
-    google = ">= 4.61, < 5.0"
+    google = ">= 4.62, < 5.0"
   }
   provider_meta "google" {
     module_name = "blueprints/terraform/terraform-google-vm:compute_instance/v8.0.0"
