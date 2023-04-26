1fdb4b517a1c26cb7d341ff7bc3294ce592ec635
Ed Barberis <ed.barberis@gmail.com>
2023-04-26 09:56:19 -0400

    Upgraded Git to 2.40.1.

--- provisioners/scripts/centos/install_centos8_git.sh
+++ provisioners/scripts/centos/install_centos8_git.sh
@@ -23,7 +23,7 @@ dnf -y install gcc perl-ExtUtils-MakeMaker
 
 # install git binaries from source. ----------------------------------------------------------------
 githome="git"
-gitrelease="2.40.0"
+gitrelease="2.40.1"
 gitfolder="git-${gitrelease}"
 gitbinary="${gitfolder}.tar.gz"
 
