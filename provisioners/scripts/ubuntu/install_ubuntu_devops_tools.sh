f506fff1e9e3ac6b7c070b6365562831249cd23d
Ed Barberis <ed.barberis@gmail.com>
2023-05-19 15:38:01 -0400

    Added support for Ubuntu 23.04 Linux 64-bit.

--- provisioners/scripts/ubuntu/install_ubuntu_devops_tools.sh
+++ provisioners/scripts/ubuntu/install_ubuntu_devops_tools.sh
@@ -40,41 +40,63 @@ ubuntu_release=$(lsb_release -rs)
 # modify python 2.x installation command based on ubuntu release.
 if [ -n "$ubuntu_release" ]; then
   case $ubuntu_release in
-      # install python 2.x, pip2, and setuptools.
-      16.04|18.04)
-        # install python2.
-        apt-get -y install python
-        python --version
+    # install python 2.x, pip2, and setuptools.
+    16.04|18.04)
+      # install python2.
+      apt-get -y install python
+      python --version
 
-        # install pip2.
-        apt-get -y install python-pip
-        pip --version
+      # install pip2.
+      apt-get -y install python-pip
+      pip --version
 
-        # upgrade pip2 in the user's home account.
-        runuser -c "PATH=/home/${user_name}/.local/bin:/usr/local/bin:${PATH} pip install pip --upgrade --user" - ${user_name}
-        runuser -c "PATH=/home/${user_name}/.local/bin:/usr/local/bin:${PATH} pip --version" - ${user_name}
+      # upgrade pip2 in the user's home account.
+      runuser -c "PATH=/home/${user_name}/.local/bin:/usr/local/bin:${PATH} pip install pip --upgrade --user" - ${user_name}
+      runuser -c "PATH=/home/${user_name}/.local/bin:/usr/local/bin:${PATH} pip --version" - ${user_name}
 
-        # install python 2.x setup tools.
-        apt-get -y install python-setuptools
+      # install python 2.x setup tools.
+      apt-get -y install python-setuptools
 
-        # upgrade setuptools in the user's home account.
-        runuser -c "PATH=/home/${user_name}/.local/bin:/usr/local/bin:${PATH} pip install --upgrade setuptools --user" - ${user_name}
+      # upgrade setuptools in the user's home account.
+      runuser -c "PATH=/home/${user_name}/.local/bin:/usr/local/bin:${PATH} pip install --upgrade setuptools --user" - ${user_name}
 
-        # install pip 2.x wheel.
-        # install and upgrade wheel in the user's home account.
-        runuser -c "PATH=/home/${user_name}/.local/bin:/usr/local/bin:${PATH} pip install wheel --user" - ${user_name}
-        runuser -c "PATH=/home/${user_name}/.local/bin:/usr/local/bin:${PATH} pip install --upgrade wheel --user" - ${user_name}
-        ;;
-      # install python 2.x only.
-      20.04|22.04|22.10)
-        # install python2.
-        apt-get -y install python2
-        python2 --version
-        ;;
-      *)
-        echo "Error: Python2 installation NOT supported on Ubuntu release: '$(lsb_release -ds)'."
-        exit 1
-        ;;
+      # install pip 2.x wheel.
+      # install and upgrade wheel in the user's home account.
+      runuser -c "PATH=/home/${user_name}/.local/bin:/usr/local/bin:${PATH} pip install wheel --user" - ${user_name}
+      runuser -c "PATH=/home/${user_name}/.local/bin:/usr/local/bin:${PATH} pip install --upgrade wheel --user" - ${user_name}
+      ;;
+
+    # install python 2.x only.
+    20.04|22.04|22.10)
+      # install python2.
+      apt-get -y install python2
+      python2 --version
+      ;;
+
+    # skip python 2.x installation.
+    23.04)
+      ;;
+
+    *)
+      echo "Error: Python2 installation NOT supported on Ubuntu release: '$(lsb_release -ds)'."
+      exit 1
+      ;;
+  esac
+fi
+
+# modify python 3.x installation command based on ubuntu release.
+if [ -n "$ubuntu_release" ]; then
+  case $ubuntu_release in
+    23.04)
+      apt-get -y install python3
+      apt-get -y install python3-pip
+      apt-get -y install python3-full
+      python3 --version
+      pip3 --version
+
+      apt-get -y install python3-setuptools
+      apt-get -y install python3-wheel
+      ;;
   esac
 fi
 
