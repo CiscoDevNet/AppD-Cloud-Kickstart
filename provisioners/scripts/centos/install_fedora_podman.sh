c1b84b2b94ab6c8317939a053a40b1fb7b7bc566
Ed Barberis <ed.barberis@gmail.com>
2023-05-26 12:05:10 -0400

    Updated support for Fedora 34 (Cloud Edition) Linux 64-bit.

---
+++ provisioners/scripts/centos/install_fedora_podman.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -eux
+# install the podman container tools on fedora 34 (cloud edition).
+#
+# The Container Tools encompass software packages required to build and run Linux Containers on
+# RHEL-based Linux operating systems. The primary piece of software core to this set is Podman,
+# but there are many other pieces of software including, but not limited to Buildah and Skopeo.
+#
+# https://access.redhat.com/support/policy/updates/containertools
+
+# install podman container tools. ------------------------------------------------------------------
+dnf -y install buildah skopeo podman
+
+# install docker-like syntax for the 'podman' command.
+dnf -y install podman-docker
+
+# display configuration info and verify version. ---------------------------------------------------
+podman info
+podman version
+podman --version
