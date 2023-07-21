1031e4aa15c5001e58dc65597b6b7aff06013190
Ed Barberis <ed.barberis@gmail.com>
2023-07-21 14:42:34 -0400

    Upgraded AppDynamics Java Agent JDK8+ to 23.6.1.34894.

--- provisioners/scripts/common/install_appdynamics_java_agent.sh
+++ provisioners/scripts/common/install_appdynamics_java_agent.sh
@@ -21,8 +21,8 @@ appd_controller_root_password="${appd_controller_root_password:-welcome1}"
 set -x  # turn command display back ON.
 appd_java_agent_home="${appd_java_agent_home:-appagent}"
 appd_java_agent_user="${appd_java_agent_user:-centos}"
-appd_java_agent_release="${appd_java_agent_release:-23.6.0.34839}"
-appd_java_agent_sha256="${appd_java_agent_sha256:-c9f85c78032b9438e671f2c532ec22bea49724539a81208496a2ea1789feefc1}"
+appd_java_agent_release="${appd_java_agent_release:-23.6.1.34894}"
+appd_java_agent_sha256="${appd_java_agent_sha256:-ca4685059ad98c704851259716a39ef6e751afcd265122bcbbc1ccfed95c91d8}"
 
 # [OPTIONAL] appdynamics java agent config parameters [w/ defaults].
 appd_java_agent_config="${appd_java_agent_config:-false}"
@@ -52,9 +52,9 @@ Usage:
     [root]# export appd_controller_root_password="welcome1"             # [optional] controller root password (defaults to 'welcome1').
     [root]# export appd_java_agent_home="appagent"                      # [optional] java agent home (defaults to 'appagent').
     [root]# export appd_java_agent_user="centos"                        # [optional] java agent user (defaults to user 'centos').
-    [root]# export appd_java_agent_release="23.6.0.34839"               # [optional] java agent release (defaults to '23.6.0.34839').
+    [root]# export appd_java_agent_release="23.6.1.34894"               # [optional] java agent release (defaults to '23.6.1.34894').
                                                                         # [optional] java agent sha-256 checksum (defaults to published value).
-    [root]# export appd_java_agent_sha256="c9f85c78032b9438e671f2c532ec22bea49724539a81208496a2ea1789feefc1"
+    [root]# export appd_java_agent_sha256="ca4685059ad98c704851259716a39ef6e751afcd265122bcbbc1ccfed95c91d8"
 
   [OPTIONAL] appdynamics java agent config parameters [w/ defaults].
     [root]# export appd_java_agent_config="true"                        # [optional] configure appd java agent? [boolean] (defaults to 'false').
@@ -66,7 +66,7 @@ Usage:
           environment.
 
           In either case, you will need to validate the configuration before starting the Java Agent. The
-          configuration file can be found here: '<java_agent_home>/appagent/ver23.6.0.34839/conf/controller-info.xml'
+          configuration file can be found here: '<java_agent_home>/appagent/ver23.6.1.34894/conf/controller-info.xml'
 
     [root]# export appd_controller_host="apm"                           # [optional] controller host (defaults to 'apm').
     [root]# export appd_controller_port="8090"                          # [optional] controller port (defaults to '8090').
