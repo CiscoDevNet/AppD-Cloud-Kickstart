b0a9656f99c0e79802847ed845cdd6307e5f062b
Ed Barberis <ed.barberis@gmail.com>
2023-09-13 16:55:38 -0400

    Upgraded AppDynamics Java Agent JDK8+ to 23.8.0.35032.

--- provisioners/scripts/aws/initialize_al2_ext_cloud_init.sh
+++ provisioners/scripts/aws/initialize_al2_ext_cloud_init.sh
@@ -25,7 +25,7 @@ appd_controller_account_access_key="${appd_controller_account_access_key:-abcdef
 
 # [OPTIONAL] override appdynamics java agent config parameters.
 appd_java_agent_home="${appd_java_agent_home:-appagent}"
-appd_java_agent_release="${appd_java_agent_release:-23.7.0.34957}"
+appd_java_agent_release="${appd_java_agent_release:-23.8.0.35032}"
 
 # [OPTIONAL] override appdynamics machine agent config parameters.
 appd_machine_agent_home="${appd_machine_agent_home:-machine-agent}"
--- provisioners/scripts/common/install_appdynamics_java_agent.sh
+++ provisioners/scripts/common/install_appdynamics_java_agent.sh
@@ -21,8 +21,8 @@ appd_controller_root_password="${appd_controller_root_password:-welcome1}"
 set -x  # turn command display back ON.
 appd_java_agent_home="${appd_java_agent_home:-appagent}"
 appd_java_agent_user="${appd_java_agent_user:-centos}"
-appd_java_agent_release="${appd_java_agent_release:-23.7.0.34957}"
-appd_java_agent_sha256="${appd_java_agent_sha256:-1b19b4107317133598fff8668cd69dfe09a3599e9d6f4aa82059c49605947322}"
+appd_java_agent_release="${appd_java_agent_release:-23.8.0.35032}"
+appd_java_agent_sha256="${appd_java_agent_sha256:-bdb790454d44c21dab74bf400842739866c86faa9568bd95b35be1bc3b4c9c93}"
 
 # [OPTIONAL] appdynamics java agent config parameters [w/ defaults].
 appd_java_agent_config="${appd_java_agent_config:-false}"
@@ -52,9 +52,9 @@ Usage:
     [root]# export appd_controller_root_password="welcome1"             # [optional] controller root password (defaults to 'welcome1').
     [root]# export appd_java_agent_home="appagent"                      # [optional] java agent home (defaults to 'appagent').
     [root]# export appd_java_agent_user="centos"                        # [optional] java agent user (defaults to user 'centos').
-    [root]# export appd_java_agent_release="23.7.0.34957"               # [optional] java agent release (defaults to '23.7.0.34957').
+    [root]# export appd_java_agent_release="23.8.0.35032"               # [optional] java agent release (defaults to '23.8.0.35032').
                                                                         # [optional] java agent sha-256 checksum (defaults to published value).
-    [root]# export appd_java_agent_sha256="1b19b4107317133598fff8668cd69dfe09a3599e9d6f4aa82059c49605947322"
+    [root]# export appd_java_agent_sha256="bdb790454d44c21dab74bf400842739866c86faa9568bd95b35be1bc3b4c9c93"
 
   [OPTIONAL] appdynamics java agent config parameters [w/ defaults].
     [root]# export appd_java_agent_config="true"                        # [optional] configure appd java agent? [boolean] (defaults to 'false').
@@ -66,7 +66,7 @@ Usage:
           environment.
 
           In either case, you will need to validate the configuration before starting the Java Agent. The
-          configuration file can be found here: '<java_agent_home>/appagent/ver23.7.0.34957/conf/controller-info.xml'
+          configuration file can be found here: '<java_agent_home>/appagent/ver23.8.0.35032/conf/controller-info.xml'
 
     [root]# export appd_controller_host="apm"                           # [optional] controller host (defaults to 'apm').
     [root]# export appd_controller_port="8090"                          # [optional] controller port (defaults to '8090').
