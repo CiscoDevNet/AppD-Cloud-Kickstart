4d029bd6efb48572a540e3191f044bf51ffca731
Ed Barberis <ed.barberis@gmail.com>
2023-12-07 14:16:42 -0500

    Updated AWS 'kubectl' to '1.28.3' [2023-11-14].

--- provisioners/scripts/common/install_aws_kubectl_cli.sh
+++ provisioners/scripts/common/install_aws_kubectl_cli.sh
@@ -15,21 +15,24 @@
 #---------------------------------------------------------------------------------------------------
 
 # install kubectl cli. -----------------------------------------------------------------------------
-kubectl_release="1.27.4"
-kubectl_date="2023-08-16"
-kubectl_sha256="e761d2a253c2fb2b2ecd506fa54f878e30057000f8b20ec7ef88f6112f3ac845"
-#kubectl_release="1.26.7"
-#kubectl_date="2023-08-16"
-#kubectl_sha256="634bb5b1c50922f43521defc4eedab80bbf5485027a0a700202ae7b996bce6cb"
-#kubectl_release="1.25.12"
-#kubectl_date="2023-08-16"
-#kubectl_sha256="69478afcead0a5df3164f4a2923ee9bb74e7d81ac3facf90491b02e169f1d0a9"
-#kubectl_release="1.24.16"
-#kubectl_date="2023-08-16"
-#kubectl_sha256="bd65a65f83c15d1946149df4c9dfe2d9b6e637cdc2afdd87826d3279a16f0faf"
+kubectl_release="1.28.3"
+kubectl_date="2023-11-14"
+kubectl_sha256="3b9ffe2effbfc12a30b12739126f069fe8a7f13625e71ccb82c33ad1ea8f8092"
+#kubectl_release="1.27.7"
+#kubectl_date="2023-11-14"
+#kubectl_sha256="d2351de8ebcebb4a3ba1a3daf359910b93460a0b71db93f4f83ddfabcd0ed041"
+#kubectl_release="1.26.10"
+#kubectl_date="2023-11-14"
+#kubectl_sha256="b55a9aa731e79b824812555c0f8671e00e03b3e2a41403134b8c2f883506fdb6"
+#kubectl_release="1.25.15"
+#kubectl_date="2023-11-14"
+#kubectl_sha256="d08fe44203e0798ec34d7cd4721202bebafc9b3f3b5bca5c9a62f15e58423260"
+#kubectl_release="1.24.17"
+#kubectl_date="2023-11-14"
+#kubectl_sha256="ef774c10dbfbff8a593c8761796794e5f7ef19f8fd4672c6321b0fa5722a76be"
 #kubectl_release="1.23.17"
-#kubectl_date="2023-08-16"
-#kubectl_sha256="f449567ba30740c5f06428466a1d2a4562d1d2fdf2260a1ebe10dd8fb28a28ce"
+#kubectl_date="2023-11-14"
+#kubectl_sha256="8ebc59c645d59b2b3f4d69485ef4abbd1f3b9461a7e5cf7222a5c85abc3f5aee"
 
 # create local bin directory (if needed).
 mkdir -p /usr/local/bin
@@ -52,6 +55,13 @@ PATH=/usr/local/bin:$PATH
 export PATH
 
 # verify installation.
-kubectl version --short --client
+case $kubectl_release in
+  1.28.3|1.29.0)
+    kubectl version --client
+    ;;
+  *)
+    kubectl version --short --client
+    ;;
+esac
 
 #export KUBECONFIG=$KUBECONFIG:$HOME/.kube/config
