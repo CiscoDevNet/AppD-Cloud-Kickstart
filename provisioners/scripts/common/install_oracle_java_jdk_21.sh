f6bab0f808a4c3b22cf2d5a167d52138000eafbe
Ed Barberis <ed.barberis@gmail.com>
2023-12-07 14:16:42 -0500

    Upgraded support for Oracle Java SE JDK 17.0.9 and 21.0.1.

--- provisioners/scripts/common/install_oracle_java_jdk_21.sh
+++ provisioners/scripts/common/install_oracle_java_jdk_21.sh
@@ -1,25 +1,25 @@
 #!/bin/sh -eux
-# install java se 17 development kit by oracle.
+# install java se 21 development kit by oracle.
 
-# install java se 17 development kit. --------------------------------------------------------------
-jdk_home="jdk17"
-jdk_build="17.0.9"
+# install java se 21 development kit. --------------------------------------------------------------
+jdk_home="jdk21"
+jdk_build="21.0.1"
 jdk_folder="jdk-${jdk_build}"
 jdk_binary="${jdk_folder}_linux-x64_bin.tar.gz"
-jdk_sha256="ad45ac97b3bc65497376f98ee276f84f4ab55ef2f62ab7f82ac0013e5b17744a"
+jdk_sha256="7c1f7689db0f4b48ee6978029c4a1aecd1442a8a7637cdf43a5471d0c79712a8"
 
 # create java home parent folder.
 mkdir -p /usr/local/java
 cd /usr/local/java
 
-# download jdk 17 binary from oracle otn.
+# download jdk 21 binary from oracle otn.
 wget --no-verbose https://download.oracle.com/java/${jdk_build:0:2}/archive/${jdk_binary}
 
 # verify the downloaded binary.
 echo "${jdk_sha256} ${jdk_binary}" | sha256sum --check
 # ${jdk_folder}_linux-x64_bin.tar.gz: OK
 
-# extract jdk 17 binary and create softlink to 'jdk17'.
+# extract jdk 21 binary and create softlink to 'jdk21'.
 rm -f ${jdk_home}
 rm -rf ${jdk_folder}
 tar -zxvf ${jdk_binary} --no-same-owner --no-overwrite-dir
@@ -27,7 +27,7 @@ chown -R root:root ./${jdk_folder}
 ln -s ${jdk_folder} ${jdk_home}
 rm -f ${jdk_binary}
 
-# set jdk 17 home environment variables.
+# set jdk 21 home environment variables.
 JAVA_HOME=/usr/local/java/${jdk_home}
 export JAVA_HOME
 PATH=${JAVA_HOME}/bin:$PATH
