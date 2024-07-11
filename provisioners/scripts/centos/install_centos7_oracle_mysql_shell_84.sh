3b0ba411869c863d6d4f7191c9e5607502ae7da2
Ed Barberis <ed.barberis@gmail.com>
2024-07-11 15:26:19 -0400

    Added support for MySQL Shell version 8.4.1 LTS.

---
+++ provisioners/scripts/centos/install_centos7_oracle_mysql_shell_84.sh
@@ -0,0 +1,45 @@
+#!/bin/sh -eux
+# install oracle mysql shell 8.4 lts command-line utility on centos linux 7.x.
+
+# set default value for appdynamics cloud kickstart home environment variable if not set. ----------
+kickstart_home="${kickstart_home:-/opt/appd-cloud-kickstart}"   # [optional] kickstart home (defaults to '/opt/appd-cloud-kickstart').
+
+# create scripts directory (if needed). ------------------------------------------------------------
+mkdir -p ${kickstart_home}/provisioners/scripts/centos
+cd ${kickstart_home}/provisioners/scripts/centos
+
+# install mysql shell. -----------------------------------------------------------------------------
+mysqlsh_release="8.4.1-1"
+mysqlsh_binary="mysql-shell-${mysqlsh_release}.el7.x86_64.rpm"
+mysqlsh_checksum="6e1c80d45449daae84d6942c18cdbc84"
+
+# download mysql shell repository.
+rm -f ${mysqlsh_binary}
+wget --no-verbose --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" https://dev.mysql.com/get/Downloads/MySQL-Shell/${mysqlsh_binary}
+
+# verify the downloaded binary using the md5 checksum.
+echo "${mysqlsh_checksum} ${mysqlsh_binary}" | md5sum --check -
+# mysql-shell-${mysqlsh_release}.el7.x86_64.rpm: OK
+
+# install mysql shell. -----------------------------------------------------------------------------
+yum -y install ${mysqlsh_binary}
+
+# verify mysql shell installation.
+mysqlsh --version
+
+# mysqlsh command-line examples. -------------------------------------------------------------------
+# MySQL Shell User Guide
+#   This is the MySQL Shell User Guide extract from the MySQL 8.4 Reference Manual.
+#   https://dev.mysql.com/doc/mysql-shell-excerpt/8.4/en/
+#
+#   For help with using MySQL, please visit either the MySQL Forums or MySQL Mailing Lists,
+#   where you can discuss your issues with other MySQL users.
+#
+# Example Usage:
+#   $ mysqlsh
+#   MySQL JS > \connect root@localhost:3306
+#   MySQL JS > \sql
+#   MySQL JS > show databases;
+#   MySQL JS > \use mysql
+#   MySQL JS > show tables;
+#   MySQL JS > \exit
