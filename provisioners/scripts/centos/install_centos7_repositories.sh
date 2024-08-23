104e21325f2a4ff1a21e6f3720a6bd0cef4de87c
Ed Barberis <ed.barberis@gmail.com>
2024-08-23 15:04:06 -0400

    Reconfigured CentOS Linux 7.9 to use EPEL archive repositories.

--- provisioners/scripts/centos/install_centos7_repositories.sh
+++ provisioners/scripts/centos/install_centos7_repositories.sh
@@ -3,9 +3,12 @@
 
 # install epel repository if needed. ---------------------------------------------------------------
 if [ ! -f "/etc/yum.repos.d/epel.repo" ]; then
-  rm -f /tmp/epel-release-latest-7.noarch.rpm
-  curl --silent --location https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm --output /tmp/epel-release-latest-7.noarch.rpm
-  yum -y install /tmp/epel-release-latest-7.noarch.rpm
+  rm -f /tmp/epel-release-7-14.noarch.rpm
+# rm -f /tmp/epel-release-latest-7.noarch.rpm
+  curl --silent --location https://archives.fedoraproject.org/pub/archive/epel/7/x86_64/Packages/e/epel-release-7-14.noarch.rpm --output /tmp/epel-release-7-14.noarch.rpm
+# curl --silent --location https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm --output /tmp/epel-release-latest-7.noarch.rpm
+  yum -y install /tmp/epel-release-7-14.noarch.rpm
+# yum -y install /tmp/epel-release-latest-7.noarch.rpm
 fi
 
 # centos linux 7 reached eol on July 1, 2024, so 'mirrorlist.centos.org' no longer exists.
--- provisioners/scripts/centos/install_centos7_xmlstarlet_xml_processor.sh
+++ provisioners/scripts/centos/install_centos7_xmlstarlet_xml_processor.sh
@@ -1,18 +1,14 @@
 #!/bin/sh -eux
 # install xmlstarlet command-line xml processor for linux 64-bit.
 
-# set default value for kickstart home environment variable if not set. ----------------------------
-kickstart_home="${kickstart_home:-/opt/appd-cloud-kickstart}"   # [optional] kickstart home (defaults to '/opt/appd-cloud-kickstart').
-
-# create scripts directory (if needed). ------------------------------------------------------------
-mkdir -p ${kickstart_home}/provisioners/scripts/centos
-cd ${kickstart_home}/provisioners/scripts/centos
-
 # install epel repository if needed. ---------------------------------------------------------------
 if [ ! -f "/etc/yum.repos.d/epel.repo" ]; then
-  rm -f install epel-release-latest-7.noarch.rpm
-  wget --no-verbose https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-  yum -y install epel-release-latest-7.noarch.rpm
+  rm -f /tmp/epel-release-7-14.noarch.rpm
+# rm -f epel-release-latest-7.noarch.rpm
+  curl --silent --location https://archives.fedoraproject.org/pub/archive/epel/7/x86_64/Packages/e/epel-release-7-14.noarch.rpm --output /tmp/epel-release-7-14.noarch.rpm
+# wget --no-verbose https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+  yum -y install /tmp/epel-release-7-14.noarch.rpm
+# yum -y install epel-release-latest-7.noarch.rpm
 fi
 
 # install xmlstarlet xml processor. ----------------------------------------------------------------
