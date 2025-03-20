fef37e6223b782f01d87a72d7a1e97dbc96eedf1
Ed Barberis <ed.barberis@gmail.com>
2025-03-20 16:28:27 -0400

    Upgraded support for Oracle Java SE JDK 21.0.6, 23.0.2, and 24.0.0.

--- provisioners/scripts/common/install_oracle_java_jdk_23.sh
+++ provisioners/scripts/common/install_oracle_java_jdk_23.sh
@@ -1,17 +1,17 @@
 #!/bin/bash -eux
 #---------------------------------------------------------------------------------------------------
-# Install Java SE 21 Development Kit by Oracle.
+# Install Java SE 23 Development Kit by Oracle.
 #
-# The Java Platform, Standard Edition Development Kit (JDK) 21 is a development environment for
+# The Java Platform, Standard Edition Development Kit (JDK) 23 is a development environment for
 # building applications and components using the Java programming language.
 #
 # The JDK includes tools useful for developing, testing, and monitoring programs written in the
 # Java programming language and running on the Java platform.
 #
 # For more details, please visit:
-#   https://docs.oracle.com/en/java/javase/21/
-#   https://www.oracle.com/java/technologies/javase/jdk21-readme-downloads.html
-#   https://www.oracle.com/java/technologies/downloads/#java21
+#   https://docs.oracle.com/en/java/javase/23/
+#   https://www.oracle.com/java/technologies/javase/jdk23-readme-downloads.html
+#   https://www.oracle.com/java/technologies/downloads/#java23
 #
 # NOTE: Script should be run with 'root' privilege.
 #---------------------------------------------------------------------------------------------------
@@ -19,19 +19,19 @@
 # retrieve the current cpu architecture. -----------------------------------------------------------
 cpu_arch=$(uname -m)
 
-# install java se 21 development kit. --------------------------------------------------------------
-jdk_home="jdk21"
-jdk_build="21.0.6"
+# install java se 23 development kit. --------------------------------------------------------------
+jdk_home="jdk23"
+jdk_build="23.0.2"
 jdk_folder="jdk-${jdk_build}"
 
 # set the jdk sha256 and arch values based on cpu architecture.
 if [ "$cpu_arch" = "x86_64" ]; then
   # set the amd64 variables.
-  jdk_sha256="e6a8125085949cb6c9fb823c79ed009a0ef24c878662c02fcb5ae890375e5a38"
+  jdk_sha256="12d7553d06b5cacf88b26cad4a8ba83cabe79646f1defb1b7fd029f3356d0922"
   jdk_arch="x64"
 elif [ "$cpu_arch" = "aarch64" ]; then
   # set the arm64 variables.
-  jdk_sha256="6f425b302dfc2b4f38d9bdcf5d62656032afdf7795796f4edff082b9e8e8d7d7"
+  jdk_sha256="3f8423bbd538e7cb20a26f944429170b47413af00ccc0e01ce5255f3f9a8d69b"
   jdk_arch="aarch64"
 else
   echo "Error: Unsupported CPU architecture: '${cpu_arch}'."
@@ -44,7 +44,7 @@ jdk_binary="${jdk_folder}_linux-${jdk_arch}_bin.tar.gz"
 mkdir -p /usr/local/java
 cd /usr/local/java
 
-# download jdk 21 binary from oracle otn.
+# download jdk 23 binary from oracle otn.
 wget --no-verbose https://download.oracle.com/java/${jdk_build:0:2}/archive/${jdk_binary}
 #wget --no-verbose https://download.oracle.com/java/${jdk_build:0:2}/latest/${jdk_binary}   # permanent (latest) url.
 
@@ -52,7 +52,7 @@ wget --no-verbose https://download.oracle.com/java/${jdk_build:0:2}/archive/${jd
 echo "${jdk_sha256} ${jdk_binary}" | sha256sum --check
 # ${jdk_folder}_linux-${jdk_arch}_bin.tar.gz: OK
 
-# extract jdk 21 binary and create softlink to 'jdk21'.
+# extract jdk 23 binary and create softlink to 'jdk23'.
 rm -f ${jdk_home}
 rm -rf ${jdk_folder}
 tar -zxvf ${jdk_binary} --no-same-owner --no-overwrite-dir
@@ -60,7 +60,7 @@ chown -R root:root ./${jdk_folder}
 ln -s ${jdk_folder} ${jdk_home}
 rm -f ${jdk_binary}
 
-# set jdk 21 home environment variables.
+# set jdk 23 home environment variables.
 JAVA_HOME=/usr/local/java/${jdk_home}
 export JAVA_HOME
 PATH=${JAVA_HOME}/bin:$PATH
--- provisioners/scripts/common/install_oracle_java_jdk_24.sh
+++ provisioners/scripts/common/install_oracle_java_jdk_24.sh
@@ -1,17 +1,17 @@
 #!/bin/bash -eux
 #---------------------------------------------------------------------------------------------------
-# Install Java SE 21 Development Kit by Oracle.
+# Install Java SE 24 Development Kit by Oracle.
 #
-# The Java Platform, Standard Edition Development Kit (JDK) 21 is a development environment for
+# The Java Platform, Standard Edition Development Kit (JDK) 24 is a development environment for
 # building applications and components using the Java programming language.
 #
 # The JDK includes tools useful for developing, testing, and monitoring programs written in the
 # Java programming language and running on the Java platform.
 #
 # For more details, please visit:
-#   https://docs.oracle.com/en/java/javase/21/
-#   https://www.oracle.com/java/technologies/javase/jdk21-readme-downloads.html
-#   https://www.oracle.com/java/technologies/downloads/#java21
+#   https://docs.oracle.com/en/java/javase/24/
+#   https://www.oracle.com/java/technologies/javase/jdk24-readme-downloads.html
+#   https://www.oracle.com/java/technologies/downloads/#java24
 #
 # NOTE: Script should be run with 'root' privilege.
 #---------------------------------------------------------------------------------------------------
@@ -19,19 +19,19 @@
 # retrieve the current cpu architecture. -----------------------------------------------------------
 cpu_arch=$(uname -m)
 
-# install java se 21 development kit. --------------------------------------------------------------
-jdk_home="jdk21"
-jdk_build="21.0.6"
-jdk_folder="jdk-${jdk_build}"
+# install java se 24 development kit. --------------------------------------------------------------
+jdk_home="jdk24"
+jdk_build="24.0.0"
+jdk_folder="jdk-${jdk_build:0:2}"
 
 # set the jdk sha256 and arch values based on cpu architecture.
 if [ "$cpu_arch" = "x86_64" ]; then
   # set the amd64 variables.
-  jdk_sha256="e6a8125085949cb6c9fb823c79ed009a0ef24c878662c02fcb5ae890375e5a38"
+  jdk_sha256="f04a4421519b9719ac96078e16af1ff0bbbfdbda7481b8ad573d75b5b6cd50b1"
   jdk_arch="x64"
 elif [ "$cpu_arch" = "aarch64" ]; then
   # set the arm64 variables.
-  jdk_sha256="6f425b302dfc2b4f38d9bdcf5d62656032afdf7795796f4edff082b9e8e8d7d7"
+  jdk_sha256="428a08439fea7c73cdc55d986faff1660afd1e40d8d275a3094cbabcda63c0ab"
   jdk_arch="aarch64"
 else
   echo "Error: Unsupported CPU architecture: '${cpu_arch}'."
@@ -44,7 +44,7 @@ jdk_binary="${jdk_folder}_linux-${jdk_arch}_bin.tar.gz"
 mkdir -p /usr/local/java
 cd /usr/local/java
 
-# download jdk 21 binary from oracle otn.
+# download jdk 24 binary from oracle otn.
 wget --no-verbose https://download.oracle.com/java/${jdk_build:0:2}/archive/${jdk_binary}
 #wget --no-verbose https://download.oracle.com/java/${jdk_build:0:2}/latest/${jdk_binary}   # permanent (latest) url.
 
@@ -52,7 +52,7 @@ wget --no-verbose https://download.oracle.com/java/${jdk_build:0:2}/archive/${jd
 echo "${jdk_sha256} ${jdk_binary}" | sha256sum --check
 # ${jdk_folder}_linux-${jdk_arch}_bin.tar.gz: OK
 
-# extract jdk 21 binary and create softlink to 'jdk21'.
+# extract jdk 24 binary and create softlink to 'jdk24'.
 rm -f ${jdk_home}
 rm -rf ${jdk_folder}
 tar -zxvf ${jdk_binary} --no-same-owner --no-overwrite-dir
@@ -60,7 +60,7 @@ chown -R root:root ./${jdk_folder}
 ln -s ${jdk_folder} ${jdk_home}
 rm -f ${jdk_binary}
 
-# set jdk 21 home environment variables.
+# set jdk 24 home environment variables.
 JAVA_HOME=/usr/local/java/${jdk_home}
 export JAVA_HOME
 PATH=${JAVA_HOME}/bin:$PATH
